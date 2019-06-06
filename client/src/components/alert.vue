ecec6f8a8e3ddb84a07d554a1a9ddefa5b5bfc9d
Jonas Tamimi <oas@users.noreply.github.com>
2019-06-06 13:34:48 +0200

    Added the first vue components

---
+++ client/src/components/alert.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="alert w-100 mb-0" v-bind:class="'alert-' + alert.state" v-html="alert.message"></div>
+</template>
+
+<script>
+	export default {
+		props: {
+			alert: {
+				message: null,
+				state: null
+			}
+		},
+		watch: {
+			alert: function () {
+				const vue = this;
+				// Removed the timeout temporarily
+				/*
+				setTimeout(function () {
+					vue.alert.message = null;
+				}, 4000);
+				*/
+			}
+		},
+	}
+</script>
---
+++ client/src/components/header.vue
@@ -0,0 +1,43 @@
+<template>
+	<nav class="navbar navbar-expand mb-4">
+		<router-link to="/" class="navbar-brand">DMN Tester</router-link>
+		<ul class="navbar-nav" v-if="this.$route.params.workspace !== undefined">
+			<li class="nav-item" v-for="(name, path) in routes">
+				<router-link active-class="active" class="nav-link" v-bind:to="'/' + $route.params.workspace + '/' + path">{{name}}</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<style>
+	.navbar {
+		background: #3b7663;
+	}
+
+	.navbar .nav-link {
+		color: rgba(255, 255, 255, 0.6);
+	}
+
+	.navbar .navbar-brand,
+	.navbar .nav-link.active {
+		color: #ffffff;
+	}
+</style>
+
+<script>
+	export default {
+		data: function () {
+			return {
+				routes: {
+					"playground": "Playground",
+					"model": "Model",
+					"builder": "Builder",
+					"inputs": "Inputs",
+					"outputs": "Outputs",
+					"tests": "Tests",
+					"settings": "Settings",
+				}
+			}
+		}
+	};
+</script>
