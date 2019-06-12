16c38c92c7bcc73ff610bc782b2eea80fb7fd988
Jonas Tamimi <oas@users.noreply.github.com>
2019-06-12 18:55:14 +0200

    Redesigned the builder, additional request to close #10

--- client/src/views/builder.vue
+++ client/src/views/builder.vue
@@ -6,13 +6,21 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-6 mb-4">
-				<h3 class="mb-2">Input</h3>
-				<div class="card">
-					<div class="card-body">
-						<pre class="mb-0"><code>{{model.input.value}}</code></pre>
+			<div class="col-8 mb-4">
+				<div class="row">
+					<div class="col-6 mb-2">
+						<h3 class="mb-0">Input</h3>
 					</div>
-					<div class="card-footer">
+					<div class="col-6 mb-2">
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text">Template</span>
+							</div>
+							<select class="form-control" v-on:change="importInput(inputs[$event.target.value].value)">
+								<option selected disabled>Select Template...</option>
+								<option v-for="(input, key) in inputs" v-bind:value="key">{{input.name}}</option>
+							</select>
+						</div>
 						<div class="input-group">
 							<input type="text" class="form-control" placeholder="Enter Name..." v-model="model.input.name">
 							<div class="input-group-append">
@@ -21,8 +29,13 @@
 						</div>
 					</div>
 				</div>
+				<div class="row">
+					<div class="col-12 mb-4">
+						<json-builder v-bind:template="model.template" v-bind:fixed="true" v-on:update:values="model.input.value = $event; getModelOutputs();"></json-builder>
+					</div>
+				</div>
 			</div>
-			<div class="col-6 mb-4">
+			<div class="col-4 mb-4">
 				<h3 class="mb-2">Outputs</h3>
 				<div class="card mb-2" v-for="(output, key) in model.outputs">
 					<div class="card-header">
@@ -40,27 +53,6 @@
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-8 mb-2">
-				<h3 class="mb-0">Builder</h3>
-			</div>
-			<div class="col-4 mb-2">
-				<div class="input-group">
-					<div class="input-group-prepend">
-						<span class="input-group-text">Template</span>
-					</div>
-					<select class="form-control" v-on:change="importInput(inputs[$event.target.value].value)">
-						<option selected disabled>Select Template...</option>
-						<option v-for="(input, key) in inputs" v-bind:value="key">{{input.name}}</option>
-					</select>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-12 mb-4">
-				<json-builder v-bind:template="model.template" v-bind:fixed="true" v-on:update:values="model.input.value = $event; getModelOutputs();"></json-builder>
 			</div>
 		</div>
 	</div>
@@ -124,6 +116,7 @@
 					name: this.model.input.name,
 					value: this.model.input.value
 				});
+				await this.getInputs();
 				this.alert = {
 					message: "The input was successfully saved.",
 					state: "success"
