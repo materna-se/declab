f51ab82166ed85c516c67065d55e14d8d1984def
Jonas Tamimi <oas@users.noreply.github.com>
2019-06-26 12:42:57 +0200

    Added alert to indicate a successful import

--- client/src/views/settings.vue
+++ client/src/views/settings.vue
@@ -1,5 +1,10 @@
 <template>
 	<div class="container-fluid">
+		<div class="row mb-4" v-if="alert.message !== null">
+			<div class="col-12">
+				<alert v-bind:alert="alert"></alert>
+			</div>
+		</div>
 		<div class="row mb-2">
 			<div class="col-12">
 				<h3 class="mb-0">Settings</h3>
@@ -29,15 +34,29 @@
 <script>
 	import Network from "../helpers/network";
 
+	import Alert from "../components/alert.vue";
+
 	export default {
+		components: {
+			"alert": Alert,
+		},
 		data() {
 			return {
+				alert: {
+					message: null,
+					state: null
+				},
+
 				endpoint: Network._endpoint,
 			};
 		},
 		methods: {
 			async importBackup(event) {
 				await Network.importBackup(event.target.files[0]);
+				this.alert = {
+					message: "The backup was successfully imported.",
+					state: "success"
+				};
 			}
 		}
 	};
