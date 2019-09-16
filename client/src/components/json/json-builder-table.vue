d359a3f25fb46621496f557b62939a84b892c5d5
Jonas Tamimi <oas@users.noreply.github.com>
2019-09-16 17:20:55 +0200

    Added support for dateTime, refactored design of the JSON builder

--- client/src/components/json/json-builder-table.vue
+++ client/src/components/json/json-builder-table.vue
@@ -1,12 +1,23 @@
 <template>
 	<div>
-		<div class="input-group" v-if="value.type === 'string' || value.type === 'date' || value.type === 'time' || value.type === 'dateTime'" v-bind:style="{'opacity': value.value === value.template ? '0.6' : '1'}">
+		<!--
+		Display value based on the data type.
+		-->
+		<div class="input-group" v-if="value.type === 'string'" v-bind:style="{'opacity': value.value === value.template ? '0.6' : '1'}">
 			<div class="input-group-prepend">
 				<span class="input-group-text">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 11a2 2 0 0 1 2 2v4H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2m-2 2v2h2v-2H4m16 0v2h2v2h-2a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2v2h-2m-8-6v4h2a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2V7h2m0 8h2v-2h-2v2z" fill="currentColor"></path></svg>
 				</span>
 			</div>
 			<input type="text" placeholder="Enter Value..." class="form-control" v-bind:value="value.value" v-bind:disabled="fixedValues" v-on:input="$set(value, 'value', $event.target.value === '' ? undefined : $event.target.value)">
+		</div>
+		<div class="input-group" v-else-if="value.type === 'dateTime'" v-bind:style="{'opacity': value.value === value.template ? '0.6' : '1'}">
+			<div class="input-group-prepend">
+				<span class="input-group-text">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 10v2H7v-2h2m4 0v2h-2v-2h2m4 0v2h-2v-2h2m2-7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h1V1h2v2h8V1h2v2h1m0 16V8H5v11h14M9 14v2H7v-2h2m4 0v2h-2v-2h2m4 0v2h-2v-2h2z" fill="currentColor"></path></svg>
+				</span>
+			</div>
+			<input type="datetime-local" class="form-control" v-bind:value="value.value" v-bind:disabled="fixedValues" v-on:input="$set(value, 'value', $event.target.value === '' ? undefined : $event.target.value)">
 		</div>
 		<div class="input-group" v-else-if="value.type === 'number'" v-bind:style="{'opacity': value.value === value.template ? '0.6' : '1'}">
 			<div class="input-group-prepend">
@@ -50,8 +61,8 @@
 						<json-builder-table v-bind:value="childValue" v-bind:fixed="fixed" v-bind:fixed-values="fixedValues"></json-builder-table>
 					</td>
 					<td class="td-minimize bg-light" v-if="!fixed">
-						<button type="button" class="btn btn-white d-block" v-on:click="$delete(value.value, childKey)">
-							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" style="margin: 3px 0">
+						<button type="button" class="btn btn-white" v-on:click="$delete(value.value, childKey)">
+							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" class="d-block" style="margin: 3px 0">
 								<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" fill="currentColor"></path>
 							</svg>
 						</button>
@@ -74,18 +85,18 @@
 						<json-builder-table v-bind:value="childValue" v-bind:fixed="fixed" v-bind:fixed-values="fixedValues"></json-builder-table>
 					</td>
 					<td class="td-minimize bg-light" v-if="!fixedValues">
-						<button type="button" class="btn btn-white d-block float-left mb-compact" v-on:click="value.value.splice(childIndex + 1, 0, JSON.parse(JSON.stringify(childValue)))">
-							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" style="transform: scale(1,-1); margin: 3px 0">
+						<button type="button" class="btn btn-white mb-compact" v-on:click="value.value.splice(childIndex + 1, 0, JSON.parse(JSON.stringify(childValue)))">
+							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" class="d-block" style="transform: scale(1,-1); margin: 3px 0">
 								<path d="M2 16h8v-2H2m16 0v-4h-2v4h-4v2h4v4h2v-4h4v-2m-8-8H2v2h12m0 2H2v2h12v-2z" fill="currentColor"></path>
 							</svg>
 						</button>
-						<button type="button" class="btn btn-white d-block float-left mb-compact" v-on:click="value.value.splice(childIndex, 0, JSON.parse(JSON.stringify(childValue)))">
-							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" style="margin: 3px 0">
+						<button type="button" class="btn btn-white mb-compact" v-on:click="value.value.splice(childIndex, 0, JSON.parse(JSON.stringify(childValue)))">
+							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" class="d-block" style="margin: 3px 0">
 								<path d="M2 16h8v-2H2m16 0v-4h-2v4h-4v2h4v4h2v-4h4v-2m-8-8H2v2h12m0 2H2v2h12v-2z" fill="currentColor"></path>
 							</svg>
 						</button>
-						<button type="button" class="btn btn-white d-block float-left" v-on:click="value.value.splice(childIndex, 1)">
-							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" style="margin: 3px 0">
+						<button type="button" class="btn btn-white" v-on:click="value.value.splice(childIndex, 1)">
+							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" class="d-block" style="margin: 3px 0">
 								<path d="M2 6v2h12V6H2m0 4v2h9v-2H2m12.17.76l-1.41 1.41L15.59 15l-2.83 2.83 1.41 1.41L17 16.41l2.83 2.83 1.41-1.41L18.41 15l2.83-2.83-1.41-1.41L17 13.59l-2.83-2.83M2 14v2h9v-2H2z" fill="currentColor"></path>
 							</svg>
 						</button>
@@ -97,39 +108,50 @@
 		<div v-else>
 			<p class="my-2 text-center text-muted">{{value.type}} is not supported yet!</p>
 		</div>
+		<!--
+		Displays the allowed values. When the respective button is clicked, it is set as a new value.
+		-->
 		<div class="input-group" v-if="value.options !== undefined && !fixedValues">
 			<button type="button" class="btn btn-white mr-compact mt-compact" v-for="option of value.options" v-on:click="$set(value, 'value', option)">{{option}}</button>
 		</div>
+		<!--
+		Create a new key-value pair or append a new element to the array.
+		-->
 		<div class="input-group mt-compact" v-if="['object', 'array'].includes(value.type) && !fixed">
 			<input class="form-control" placeholder="Enter Key..." v-model="key" v-if="!value.collection">
 			<div v-bind:class="[value.collection ? 'btn-group ml-auto': 'input-group-append']">
 				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'string')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
 						<path d="M6 11a2 2 0 0 1 2 2v4H4a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2m-2 2v2h2v-2H4m16 0v2h2v2h-2a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2v2h-2m-8-6v4h2a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2V7h2m0 8h2v-2h-2v2z" fill="currentColor"></path>
 					</svg>
 				</button>
+				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'dateTime')">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
+						<path d="M9 10v2H7v-2h2m4 0v2h-2v-2h2m4 0v2h-2v-2h2m2-7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h1V1h2v2h8V1h2v2h1m0 16V8H5v11h14M9 14v2H7v-2h2m4 0v2h-2v-2h2m4 0v2h-2v-2h2z" fill="currentColor"></path>
+					</svg>
+				</button>
 				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'number')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
 						<path d="M4 17V9H2V7h4v10H4m18-2a2 2 0 0 1-2 2h-4v-2h4v-2h-2v-2h2V9h-4V7h4a2 2 0 0 1 2 2v1.5a1.5 1.5 0 0 1-1.5 1.5 1.5 1.5 0 0 1 1.5 1.5V15m-8 0v2H8v-4a2 2 0 0 1 2-2h2V9H8V7h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-2v2h4z" fill="currentColor"></path>
 					</svg>
 				</button>
 				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'boolean')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
 						<path d="M19 19H5V5h10V3H5c-1.11 0-2 .89-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-8h-2m-11.09-.92L6.5 11.5 11 16 21 6l-1.41-1.42L11 13.17l-3.09-3.09z" fill="currentColor"></path>
 					</svg>
 				</button>
 				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'null')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16" class="d-block">
 						<path d="M15.354 1.354l-.707-.707-2.777 2.776A5.967 5.967 0 0 0 8 2C4.691 2 2 4.691 2 8c0 1.475.537 2.824 1.423 3.87L.647 14.646l.707.707 2.776-2.776A5.965 5.965 0 0 0 8 14c3.309 0 6-2.691 6-6a5.965 5.965 0 0 0-1.423-3.87l2.777-2.776zM3 8c0-2.757 2.243-5 5-5 1.198 0 2.284.441 3.146 1.146l-7 7C3.441 10.284 3 9.198 3 8zm10 0c0 2.757-2.243 5-5 5-1.198 0-2.284-.441-3.146-1.146l7-7C12.559 5.716 13 6.802 13 8z" fill="currentColor"></path>
 					</svg>
 				</button>
 				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'object')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
 						<path d="M8 3a2 2 0 0 0-2 2v4a2 2 0 0 1-2 2H3v2h1a2 2 0 0 1 2 2v4a2 2 0 0 0 2 2h2v-2H8v-5a2 2 0 0 0-2-2 2 2 0 0 0 2-2V5h2V3m6 0a2 2 0 0 1 2 2v4a2 2 0 0 0 2 2h1v2h-1a2 2 0 0 0-2 2v4a2 2 0 0 1-2 2h-2v-2h2v-5a2 2 0 0 1 2-2 2 2 0 0 1-2-2V5h-2V3h2z" fill="currentColor"></path>
 					</svg>
 				</button>
 				<button type="button" class="btn btn-white" v-on:click="addValue(value, 'array')">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
 						<path d="M7 13v-2h14v2H7m0 6v-2h14v2H7M7 7V5h14v2H7M3 8V5H2V4h2v4H3m-1 9v-1h3v4H2v-1h2v-.5H3v-1h1V17H2m2.25-7a.75.75 0 0 1 .75.75c0 .2-.08.39-.21.52L3.12 13H5v1H2v-.92L4 11H2v-1h2.25z" fill="currentColor"></path>
 					</svg>
 				</button>
@@ -162,6 +184,7 @@
 					case true:
 						switch (type) {
 							case "string":
+							case "dateTime":
 							case "number":
 							case "boolean":
 								value.value.push({type: type, collection: false});
@@ -180,6 +203,7 @@
 					case false:
 						switch (type) {
 							case "string":
+							case "dateTime":
 							case "number":
 							case "boolean":
 								this.$set(value.value, this.key, {type: type, collection: false});
