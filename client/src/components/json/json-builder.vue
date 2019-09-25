58ff76250ea70128253180f4eeac7cf9e4cabee6
Jonas Tamimi <oas@users.noreply.github.com>
2019-09-25 18:41:00 +0200

    Added first version that supports raw JSON input (#20)

--- client/src/components/json/json-builder.vue
+++ client/src/components/json/json-builder.vue
@@ -1,5 +1,8 @@
 <template>
-	<json-builder-table v-bind:value="value" v-bind:root="true" v-bind:fixed="fixed" v-bind:fixed-root="fixedRoot" v-bind:fixed-values="fixedValues" v-if="value !== null"></json-builder-table>
+	<div>
+		<textarea class="form-control w-100 mb-2" v-bind:value="JSON.stringify(cleanedValue)" v-on:input="importValue($event.target.value)"></textarea>
+		<json-builder-table v-bind:value="value" v-bind:root="true" v-bind:fixed="fixed" v-bind:fixed-root="fixedRoot" v-bind:fixed-values="fixedValues" v-if="value !== null"></json-builder-table>
+	</div>
 </template>
 
 <script>
@@ -33,42 +36,42 @@
 		},
 		data: function () {
 			return {
-				value: null
+				value: null,
+				cleanedValue: null,
 			}
 		},
 		mounted() {
-			this.value = this.enrichTemplate(this.template);
-			this.returnValue(this.value);
+			this.value = this.convert ? this.enrichTemplate(this.template) : this.template;
+			this.exportValue(this.value);
 		},
 		watch: {
 			template: function (template) {
-				this.value = this.enrichTemplate(template);
-				this.returnValue(this.value);
+				this.value = this.convert ? this.enrichTemplate(template) : template;
+				this.exportValue(this.value);
 			},
 			value: {
 				handler: function (value) {
-					this.returnValue(value);
+					console.info("handler", value);
+					this.exportValue(value);
 				},
 				deep: true
 			}
 		},
 		methods: {
 			enrichTemplate(template) {
-				if (this.convert) {
-					return Converter.enrich(template);
-				}
-
-				return template;
+				return Converter.enrich(template);
 			},
 			cleanValue(value) {
 				const cleanedObject = Converter.clean(value);
 				return cleanedObject === undefined ? {} : cleanedObject;
 			},
-			returnValue(value) {
-				/**
-				 * Template muss irgendwie über <textarea> verändert werden...
-				 */
-				this.$emit('update:value', this.cleanValue(value));
+			importValue(value) {
+				this.value = this.enrichTemplate(JSON.parse(value));
+				this.exportValue(this.value);
+			},
+			exportValue(value) {
+				this.cleanedValue = this.cleanValue(value);
+				this.$emit('update:value', this.cleanedValue);
 			}
 		}
 	};
