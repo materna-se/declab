b094357131d841c99b210370a37c0081f1cfab5b
Jonas Tamimi <oas@users.noreply.github.com>
2019-10-24 14:20:16 +0200

    Refactored publisher, standardized the design to match with the remaining components

--- client/src/views/publisher.vue
+++ client/src/views/publisher.vue
@@ -1,95 +1,56 @@
 <template>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-9">
-				<div class="mb-4">
-					<div class="row mb-2">
-						<div class="col">
-							<h1 class="mb-0">Publisher</h1>
-						</div>
+			<div class="col-12">
+				<div class="row">
+					<div class="col-10 mb-2">
+						<h3 class="mb-0">Publisher</h3>
 					</div>
-					<div class="row">
-						<div class="col">
-							<div class="card card-borderless mb-2">
-								<div class="list-group">
-									<div class="list-group-item list-group-item-action c-pointer" v-for="(test, uuid) in tests" v-bind:key="uuid" v-on:click.self="changeVisibility(test)" v-bind:class="[test.result.equal ? 'bg-success' : 'bg-danger']">
-										<div class="row mb-2" v-on:click="changeVisibility(test, test.visibility)">
-											<h4 class="d-block float-left mr-4">{{test.name}}<h6>({{uuid}})</h6></h4>
+					<div class="col-1 mb-2">
+						<button class="btn btn-block btn-outline-secondary" v-on:click="expandAll">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block mx-auto">
+								<path d="M10 21v-2H6.41l4.5-4.5-1.41-1.41-4.5 4.5V14H3v7h7m4.5-10.09l4.5-4.5V10h2V3h-7v2h3.59l-4.5 4.5 1.41 1.41z" fill="currentColor"></path>
+							</svg>
+						</button>
+					</div>
+					<div class="col-1 mb-2">
+						<button class="btn btn-block btn-outline-secondary" v-on:click="collapseAll">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block mx-auto">
+								<path d="M19.5 3.09L15 7.59V4h-2v7h7V9h-3.59l4.5-4.5-1.41-1.41M4 13v2h3.59l-4.5 4.5 1.41 1.41 4.5-4.5V20h2v-7H4z" fill="currentColor"></path>
+							</svg>
+						</button>
+					</div>
+				</div>
+
+				<div class="card mb-2" v-for="(test, uuid) in tests" v-bind:key="uuid">
+					<div class="card-body">
+						<h4 class="mb-0 c-pointer" v-on:click="changeVisibility(test)">{{test.name}}</h4>
+						<div class="card mt-2 border-lg" v-if="test.visibility" v-bind:class="[test.result.equal ? 'border-success' : 'border-danger']">
+							<div class="card-body">
+								<h5 class="mb-2">Description</h5>
+								<p class="mb-4">{{test.description}}</p>
+
+								<h5 class="mb-2 c-pointer" v-on:click="changeInputVisibility(test)">Input</h5>
+								<json-builder v-if="test.inputVisibility" v-bind:template="inputs[test.input].value" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
+								<p class="text-muted mb-4" v-else>hidden</p>
+
+								<h5 class="mt-2 mb-2 c-pointer" v-on:click="changeOutputVisibility(test)">Outputs</h5>
+								<div class="card mb-0 border-lg" v-if="test.outputVisibility" v-for="(output, decision) in test.result.outputs" v-bind:key="test.uuid + '-' + decision" v-bind:class="[output.equal ? 'border-success' : 'border-danger']">
+									<div class="card-body">
+										<h6 class="mb-2">Decision</h6>
+										<p class="mb-4">{{decision}}</p>
+
+										<h6 class="mb-2">Output</h6>
+										<div style="white-space: pre">
+												<span v-for="difference of getDifference(output.expected, output.calculated)">
+													<code class="difference-minus" v-if="difference[0] === -1">{{difference[1]}}</code>
+													<code class="difference-plus" v-else-if="difference[0] === 1">{{difference[1]}}</code>
+													<code v-else>{{difference[1]}}</code>
+												</span>
 										</div>
-										<div class="card card-borderless" v-show="test.visibility">
-											<!--<div class="card card-borderless mb-2">
-												<h5 class="row col">
-													UUID: {{uuid}}
-												</h5>
-											</div>-->
-												<h5 class="645f6606-28dd-4889-b118-30b4d88b6eca">
-													Description <h6>{{test.description}}</h6>
-												</h5>
-											<div class="list-group">
-												<div class="list-group-item list-group-item-action c-pointer" v-on:click.self="changeInputVisibility(test)">
-													<div class="row mb-2" v-on:click="changeInputVisibility(test)">
-														<h5 class="d-block float-left mr-4">Input: {{inputs[test.input].name}}</h5>
-													</div>
-													<div class="card card-borderless mb-2" v-show="test.inputVis">
-														<div class="row col">
-															<json-builder v-bind:template="inputs[test.input].value" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="card card-borderless mb-2">
-												<div class="list-group">
-													<div class="list-group-item list-group-item-action c-pointer" v-on:click.self="changeOutputVisibility(test)">
-														<div class="row mb-2" v-on:click="changeOutputVisibility(test)">
-															<h5 class="d-block float-left mr-4">Outputs:</h5>
-														</div>
-														<div class="card card-borderless mb-2" v-for="(output) in Object.values(test.result.outputs)" v-bind:key="output">
-															<div class="list-group">
-																<div class="list-group-item list-group-item-action c-pointer" v-bind:class="[output.equal ? 'bg-success' : 'bg-danger']">
-																	<div class="row mb-2">
-																		<h5 class="d-block float-left mr-4">{{output.expected.name}}</h5>
-																	</div>
-																	<div class="card card-borderless mb-2" v-show="test.outputVis">
-																		<div class="card card-borderless mb-2">
-																			<h5 class="row col">
-																				Expected
-																			</h5>
-																			<div class="row col">
-																				<json-builder v-bind:template="output.expected.value" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
-																			</div>
-																		</div>
-																		<div class="card card-borderless mb-2">
-																			<h5 class="row col">
-																				Calculated
-																			</h5>
-																			<div class="row col">
-																				<json-builder v-bind:template="output.calculated" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
-																			</div>
-																		</div>
-																		<div class="card card-borderless mb-2" v-bind:class="[output.equal ? 'bg-success' : 'bg-danger']">
-																			<h5 class="row col">
-																				Equal
-																			</h5>
-																			<json-builder v-bind:template="output.equal" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
-																		</div>
-																	</div>
-																</div>
-															</div>
-														</div>
-													</div>
-												</div>
-												<div class="card card-borderless mb-2" v-bind:class="[test.result.equal ? 'bg-success' : 'bg-danger']">
-													<h5 class="row col">
-														Results
-													</h5>
-													<div class="row col">
-														<json-builder v-bind:template="test.result.equal" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
-													</div>
-												</div>
-											</div>
 										</div>
 									</div>
-								</div>
+								<p class="text-muted mb-0" v-else>hidden</p>
 							</div>
 						</div>
 					</div>
@@ -99,15 +60,12 @@
 	</div>
 </template>
 
-<style>
-
-</style>
-
 <script>
 	import Network from "../helpers/network";
 	import DiffMatchPatch from 'diff-match-patch';
+
 	import JSONBuilder from "../components/json/json-builder.vue";
-	
+
 	export default {
 		async mounted() {
 			await this.getInputs();
@@ -125,16 +83,19 @@
 				inputs: {},
 				outputs: {},
 
-				tests: [],
+				tests: {},
 
 				test: {
 					uuid: null,
 					name: null,
 					description: null,
+
 					input: null,
-					inputVis: false,
+					inputVisibility: false,
+
 					outputs: [],
-					outputVis: false,
+					outputVisibility: false,
+
 					result: {
 						outputs: [],
 						equal: false
@@ -164,30 +125,46 @@
 			async getTests() {
 				this.tests = await Network.getTests();
 				for (const uuid in this.tests) {
-					this.$set(this.tests[uuid],"visibility",false);
-					this.$set(this.tests[uuid],"inputVis",false);
-					this.$set(this.tests[uuid],"outputVis",false);
+					const test = this.tests[uuid];
+					this.$set(test,"visibility",false);
+					this.$set(test, "inputVisibility", false);
+					this.$set(test, "outputVisibility", false);
 				}
 			},
 			async executeTests() {
 				for (const uuid in this.tests) {
+					const test = this.tests[uuid];
+
 					const tempRes = await Network.executeTest(uuid)
-					this.$set(this.tests[uuid], "result", tempRes);
+					this.$set(test, "result", tempRes);
+				}
+			},
 
-					const tempEq = this.tests[uuid].result.equal;
-					this.$delete(this.tests[uuid].result, "equal");
-
-					this.$set(this.tests[uuid].result, "equal", tempEq);
+			// Helpers
+			expandAll() {
+				for (const uuid in this.tests) {
+					const test = this.tests[uuid];
+					test.visibility = true;
+					test.inputVisibility = true;
+					test.outputVisibility = true;
+				}
+			},
+			collapseAll() {
+				for (const uuid in this.tests) {
+					const test = this.tests[uuid];
+					test.visibility = false;
+					test.inputVisibility = false;
+					test.outputVisibility = false;
 				}
 			},
 			async changeVisibility(test) {
 				test.visibility = !test.visibility;
 			},
 			async changeInputVisibility(test) {
-				test.inputVis = !test.inputVis;
+				test.inputVisibility = !test.inputVisibility;
 			},
 			async changeOutputVisibility(test) {
-				test.outputVis = !test.outputVis;
+				test.outputVisibility = !test.outputVisibility;
 			},
 			getDifference(expected, calculated) {
 				return new DiffMatchPatch().diff_main(expected !== null ? JSON.stringify(expected.value, null, 3) : '', calculated !== null ? JSON.stringify(calculated.value, null, 3) : '');
