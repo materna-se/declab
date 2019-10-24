f794f8ad9d30f6e361cb38a878d91d580bed5df1
Jonas Tamimi <oas@users.noreply.github.com>
2019-10-24 14:58:32 +0200

    Enabled alert timeout after removing it temporarily

--- client/src/components/alert.vue
+++ client/src/components/alert.vue
@@ -10,16 +10,34 @@
 				state: null
 			}
 		},
+		data() {
+			return {
+				timeout: null
+			};
+		},
+		mounted() {
+			this.addTimeout();
+		},
 		watch: {
 			alert: function () {
-				const vue = this;
-				// Removed the timeout temporarily
-				/*
-				setTimeout(function () {
-					vue.alert.message = null;
-				}, 4000);
-				*/
+				this.addTimeout();
 			}
 		},
+		methods: {
+			addTimeout() {
+				const vue = this;
+
+				// We need to remove the timeout if there is already a running one.
+				if (this.timeout !== null) {
+					clearTimeout(this.timeout);
+				}
+
+				this.timeout = setTimeout(function () {
+					vue.alert.message = null;
+					console.info("null");
+					vue.timeout = null;
+				}, 4000);
+			}
+		}
 	}
 </script>
