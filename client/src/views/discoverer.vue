db13466c00e93fd17e8d8b7c9358e29636b41886
Jonas Tamimi <oas@users.noreply.github.com>
2019-11-26 15:32:58 +0100

    Discovery session is now debounced. This fixes a concurrency issue.

--- client/src/views/discoverer.vue
+++ client/src/views/discoverer.vue
@@ -10,33 +10,27 @@
 				<h4 class="mb-2">Input</h4>
 
 				<h5 class="mb-2">Template</h5>
-				<select id="form-inputs" class="form-control mb-4" v-model="input" v-on:change="initializeDiscovery">
+				<select id="form-inputs" class="form-control mb-4" v-model="input" v-on:change="initializeDiscovery(); startDebouncedDiscovery()">
 					<option v-for="(input, uuid) in inputs" v-bind:value="uuid">{{input.name}}</option>
 				</select>
 
 				<h5 class="mb-2">Selector</h5>
-				<input type="text" placeholder="Enter JSONPath..." class="form-control mb-4" v-model="options.inputSelector">
+				<input type="text" placeholder="Enter JSONPath..." class="form-control mb-4" v-model="options.inputSelector" v-on:keyup="startDebouncedDiscovery">
 
 				<h5 class="mb-2">Values</h5>
 				<div class="card mb-4">
 					<div class="card-body">
-						<feel-editor v-bind:value="options.expression" v-on:update:value="options.expression = $event"></feel-editor>
+						<feel-editor v-bind:value="options.expression" v-on:update:value="options.expression = $event; startDebouncedDiscovery()"></feel-editor>
 					</div>
 				</div>
 
 				<h4 class="mb-2">Result</h4>
 
 				<h5 class="mb-2">Decision</h5>
-				<input type="text" placeholder="Enter Decision..." class="form-control mb-4" v-model="options.resultDecision">
+				<input type="text" placeholder="Enter Decision..." class="form-control mb-4" v-model="options.resultDecision" v-on:keyup="startDebouncedDiscovery">
 
 				<h5 class="mb-2">Selector</h5>
-				<input type="text" placeholder="Enter JSONPath..." class="form-control mb-4" v-model="options.resultSelector">
-
-				<button class="btn btn-block btn-outline-secondary mb-0" v-on:click="startDiscovery">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block mx-auto">
-						<path d="M8 5.14v14l11-7-11-7z" fill="currentColor"></path>
-					</svg>
-				</button>
+				<input type="text" placeholder="Enter JSONPath..." class="form-control mb-4" v-model="options.resultSelector" v-bind:disabled="options.resultDecision === null || options.resultDecision === ''" v-on:keyup="startDebouncedDiscovery">
 			</div>
 			<div class="col-8 mb-4" v-if="input !== null">
 				<h4 class="mb-2">Input</h4>
@@ -48,11 +42,23 @@
 						<h5 class="mb-0">Discovery {{result.range.start}} - {{result.range.stop}}</h5>
 					</div>
 					<div class="card-body">
-						<h6 class="mb-2">Input</h6>
-						<json-builder class="mb-2" v-bind:template="result.input" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
+						<!--
+						<h5 class="mb-2">Input</h5>
+						<json-builder class="mb-4" v-bind:template="result.input" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
+						-->
 
-						<h6 class="mb-2">Outputs</h6>
-						<json-builder class="mb-0" v-bind:template="result.output" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
+						<h5 class="mb-0">Output</h5>
+						<!-- If options.resultDecision is not set, we'll iterate over all decisions. -->
+						<template v-if="options.resultDecision !== null && options.resultDecision !== ''">
+							<h6 class="mt-2 mb-2">{{options.resultDecision}}</h6>
+							<json-builder class="mb-0" v-bind:template="result.output" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
+						</template>
+						<template v-else>
+							<div v-for="(output, decision) in result.output" class="mt-2">
+								<h6 class="mb-2">{{decision}}</h6>
+								<json-builder class="mb-0" v-bind:template="output.value" v-bind:fixed="true" v-bind:fixed-root="true" v-bind:fixed-values="true" v-bind:convert="true"></json-builder>
+							</div>
+						</template>
 					</div>
 				</div>
 			</div>
@@ -63,6 +69,7 @@
 <script>
 	import Network from "../helpers/network";
 	import JSONPath from "jsonpath";
+	import debounce from "lodash.debounce";
 	import FEELEditor from "../components/dmn/dmn-editor.vue";
 	import JSONBuilder from "../components/json/json-builder.vue";
 
@@ -84,12 +91,15 @@
 					input: {},
 					inputSelector: "$",
 
-					expression: "[1, 2, 3]",
+					expression: "[]",
 
 					results: [],
 					resultDecision: null,
 					resultSelector: "$",
 				},
+
+				// TODO: This is just a workaround. We should abort a discovery when new values are entered.
+				debouncedDiscovery: debounce(this.startDiscovery, Number.MAX_VALUE, {leading: true, trailing: true})
 			}
 		},
 		methods: {
@@ -99,6 +109,15 @@
 
 			async initializeDiscovery() {
 				this.options.input = this.inputs[this.input].value;
+			},
+			async startDebouncedDiscovery() {
+				try {
+					await this.debouncedDiscovery();
+				}
+				catch (e) {
+					console.error(e);
+				}
+				this.debouncedDiscovery.flush();
 			},
 			async startDiscovery() {
 				// Remove all results before adding new ones
@@ -135,12 +154,17 @@
 					const lastOutput = lastResult === undefined ? undefined : lastResult.output;
 
 					// Get the selection.
-					let currentOutputSelection = currentOutput[this.options.resultDecision].value;
+					let currentOutputSelection = currentOutput;
 					let lastOutputSelection = lastOutput;
 
-					// If the result selector is not $, select it with JSONPath.
-					if (this.options.resultSelector !== "$") {
-						currentOutputSelection = JSON.stringify(JSONPath.value(currentOutputSelection, this.options.resultSelector));
+					// If a result decision is set, select it.
+					if (this.options.resultDecision !== null && this.options.resultDecision !== "") {
+						currentOutputSelection = currentOutputSelection[this.options.resultDecision].value;
+
+						// If the result selector is not $, select it with JSONPath.
+						if (this.options.resultSelector !== "$") {
+							currentOutputSelection = JSONPath.value(currentOutputSelection, this.options.resultSelector);
+						}
 					}
 
 					// Check if the current output is the same as the last output. If it is, we'll create a new result set.
