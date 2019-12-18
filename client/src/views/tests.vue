f2d8dac9ef98334bb76f737626c02eca1218abe4
Jonas Tamimi <oas@users.noreply.github.com>
2019-12-18 12:46:21 +0100

    Added builder to the test results

--- client/src/views/tests.vue
+++ client/src/views/tests.vue
@@ -7,7 +7,7 @@
 			<div class="col-1">
 				<button class="btn btn-block btn-outline-secondary" v-on:click="setAddMode">
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block mx-auto">
-						<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="currentColor"></path>
+						<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="currentColor"/>
 					</svg>
 				</button>
 			</div>
@@ -18,14 +18,14 @@
 					<div class="col-6">
 						<button class="btn btn-block btn-outline-secondary mb-2" v-on:click="executeTests">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block mx-auto">
-								<path d="M12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10 10 10 0 0 0 10-10A10 10 0 0 0 12 2m-2 14.5l6-4.5-6-4.5v9z" fill="currentColor"></path>
+								<path d="M12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10 10 10 0 0 0 10-10A10 10 0 0 0 12 2m-2 14.5l6-4.5-6-4.5v9z" fill="currentColor"/>
 							</svg>
 						</button>
 					</div>
 					<div class="col-6">
 						<button class="btn btn-block btn-outline-secondary mb-2" v-on:click="filterTests">
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block mx-auto">
-								<path d="M15 19.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L9.29 16.7a.989.989 0 0 1-.29-.83v-5.12L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L15 10.75v9.13M7.04 5L11 10.06v5.52l2 2v-7.53L16.96 5H7.04z" fill="currentColor"></path>
+								<path d="M15 19.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L9.29 16.7a.989.989 0 0 1-.29-.83v-5.12L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L15 10.75v9.13M7.04 5L11 10.06v5.52l2 2v-7.53L16.96 5H7.04z" fill="currentColor"/>
 							</svg>
 						</button>
 					</div>
@@ -36,13 +36,13 @@
 						<div class="float-right">
 							<span class="badge badge-light badge-md d-block float-left mr-2" v-if="test.tps !== undefined">{{test.tps}} / s</span>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block float-left mr-2" v-on:click="setEditMode(uuid)">
-								<path d="M14.06 9l.94.94L5.92 19H5v-.92L14.06 9m3.6-6c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29m-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" fill="currentColor"></path>
+								<path d="M14.06 9l.94.94L5.92 19H5v-.92L14.06 9m3.6-6c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29m-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z" fill="currentColor"/>
 							</svg>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block float-left mr-2" v-on:click="setDuplicateMode(uuid)">
-								<path d="M11 17H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h12v2H4v12h7v-2l4 3-4 3v-2m8 4V7H8v6H6V7a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-2h2v2h11z" fill="currentColor"></path>
+								<path d="M11 17H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h12v2H4v12h7v-2l4 3-4 3v-2m8 4V7H8v6H6V7a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-2h2v2h11z" fill="currentColor"/>
 							</svg>
 							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block float-left" v-on:click="deleteTest(uuid)">
-								<path d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12M8 9h8v10H8V9m7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z" fill="currentColor"></path>
+								<path d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12M8 9h8v10H8V9m7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z" fill="currentColor"/>
 							</svg>
 						</div>
 					</div>
@@ -68,7 +68,7 @@
 										<h5 class="mb-2">Name</h5>
 										<p class="mb-4">{{inputs[test.input].name}}</p>
 										<h5 class="mb-2">Input</h5>
-										<pre class="mb-0"><code>{{JSON.stringify(inputs[test.input].value, null, 3)}}</code></pre>
+										<json-builder v-bind:template="inputs[test.input].value" v-bind:convert="true" v-bind:fixed="true" v-bind:fixed-values="true"></json-builder>
 									</div>
 								</div>
 							</div>
@@ -105,7 +105,7 @@
 						<input class="form-control mb-4" v-model="test.name">
 
 						<h5 class="mb-2">Description</h5>
-						<textarea rows="3" class="form-control mb-4" v-model="test.description"></textarea>
+						<textarea rows="3" class="form-control mb-4" v-model="test.description"/>
 
 						<h5 class="mb-2">Input</h5>
 						<select id="form-inputs" class="form-control mb-4" v-model="test.input">
@@ -137,8 +137,12 @@
 <script>
 	import Network from "../helpers/network";
 	import DiffMatchPatch from 'diff-match-patch';
+	import JSONBuilder from "../components/json/json-builder.vue";
 
 	export default {
+		components: {
+			"json-builder": JSONBuilder
+		},
 		async mounted() {
 			await this.getInputs();
 			await this.getOutputs();
