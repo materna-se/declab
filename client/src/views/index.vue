34aa75a0db12b80e72a2deee6d9201370c962cbd
Jonas Tamimi <oas@users.noreply.github.com>
2020-01-13 12:39:10 +0100

    Switched index.vue design in preparation for the new workspace configuration

--- client/src/views/index.vue
+++ client/src/views/index.vue
@@ -1,38 +1,68 @@
 <template>
-	<div class="login-container">
-		<h3 class="mb-2">Workspace</h3>
-		<div class="alert alert-warning mb-2">
-			This version is not stable. Please export your workspace regularly!
+	<div>
+		<div class="row">
+			<div class="col-4">
+			</div>
+			<div class="col-4 mb-4">
+				<h4 class="mb-2">Workspaces</h4>
+
+				<div class="list-group mb-2">
+					<div class="list-group-item d-flex">
+						<input placeholder="Enter Workspace Name..." class="form-control flex-fill mr-2" v-model="workspace.name" v-on:keydown.enter="enterWorkspace">
+						<button class="btn btn-outline-secondary" v-on:click="enterWorkspace">
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block">
+								<path d="M8.59 16.58L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.42z" fill="currentColor"/>
+							</svg>
+						</button>
+					</div>
+				</div>
+
+				<div class="list-group">
+					<div class="list-group-item">
+						<empty-collection/>
+					</div>
+					<!--
+					<div class="list-group-item list-group-item-action c-pointer d-flex align-items-center" v-for="d of [1,2,3,4,5,6,7,8,9]">
+						<div class="mr-auto">
+							<p class="mb-0"><b>Jonas {{d}}</b></p>
+							<p class="mb-0">Dies ist die Beschreibung von {{d}}</p>
+						</div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="d-block float-left">
+							<path d="M12 17a2 2 0 01-2-2c0-1.11.89-2 2-2a2 2 0 012 2 2 2 0 01-2 2m6 3V10H6v10h12m0-12a2 2 0 012 2v10a2 2 0 01-2 2H6a2 2 0 01-2-2V10c0-1.11.89-2 2-2h1V6a5 5 0 015-5 5 5 0 015 5v2h1m-6-5a3 3 0 00-3 3v2h6V6a3 3 0 00-3-3z" fill="currentColor"/>
+						</svg>
+					</div>
+					-->
+				</div>
+			</div>
+			<div class="col-4">
+			</div>
 		</div>
-		<p class="mb-2">If the workspace does not exist yet, it is created implicitly. To collaborate, you can share your link with others.</p>
-		<input placeholder="Enter Workspace..." class="form-control mb-2" v-model="workspace" v-on:keydown.enter="enterWorkspace">
-		<button class="btn btn-block btn-outline-secondary" v-on:click="enterWorkspace">Enter Workspace</button>
 	</div>
 </template>
 
-<style>
-	.login-container {
-		width: 100%;
-		max-width: 500px;
-		margin: auto;
-		padding: 15px;
-	}
-</style>
-
 <script>
+	import EmptyCollectionComponent from "../components/empty-collection.vue";
+
 	export default {
+		components: {
+			"empty-collection": EmptyCollectionComponent
+		},
 		data() {
 			return {
-				workspace: null
+				workspace: {
+					visible: false,
+					name: null
+				},
+				workspaces: []
 			}
 		},
 		methods: {
 			async enterWorkspace() {
-				if (this.workspace === null || this.workspace === "") {
+				if (this.workspace.name === null || this.workspace.name === "") {
 					return;
 				}
 
-				await this.$router.push('/' + this.workspace + '/model');
+				await this.$router.push('/' + this.workspace.name + '/model');
 			}
 		}
 	};
