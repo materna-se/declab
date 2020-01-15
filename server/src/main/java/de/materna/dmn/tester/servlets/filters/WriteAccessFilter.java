53942ad6eed468e828d7d6c80bf7fe87b8800d5d
Jonas Tamimi <oas@users.noreply.github.com>
2020-01-15 13:51:52 +0100

    Refactored access filters, pattern matching and token validation is no longer duplicated

--- server/src/main/java/de/materna/dmn/tester/servlets/filters/WriteAccessFilter.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/filters/WriteAccessFilter.java
@@ -1,100 +1,43 @@
 package de.materna.dmn.tester.servlets.filters;
 
-import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
+import de.materna.dmn.tester.helpers.ByteHelper;
+import de.materna.dmn.tester.persistence.WorkspaceManager;
+import de.materna.dmn.tester.servlets.filters.helpers.AccessFilterHelper;
 import de.materna.dmn.tester.servlets.workspace.beans.Configuration.Access;
-
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
+import org.apache.log4j.Logger;
 
 import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
-import javax.ws.rs.Priorities;
-
-import de.materna.dmn.tester.helpers.ByteHelper;
-import de.materna.dmn.tester.persistence.WorkspaceManager;
+import java.io.IOException;
+import java.security.MessageDigest;
 
 @WriteAccess
 @Provider
 @Priority(Priorities.AUTHENTICATION)
 public class WriteAccessFilter implements ContainerRequestFilter {
-    private static final String REALM = "example";
-    private static final String AUTHENTICATION_SCHEME = "Bearer";
+    private static final Logger log = Logger.getLogger(ReadAccessFilter.class);
     private static final WorkspaceManager workspaceManager = WorkspaceManager.getInstance();
 
     @Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-
-        // Get the workspace name from the request
-    	String urlString = requestContext.getUriInfo().getRequestUri().toURL().getPath();
-        Pattern pattern = Pattern.compile("api/workspaces/(.*?)/");
-        Matcher matcher = pattern.matcher(urlString);
-        if(!matcher.find()) {
-        	requestContext.abortWith(Response.status(Response.Status.BAD_REQUEST).build());
-        }
-        String workspaceName = matcher.group(1);
-        
-        //Get workspace object
-        Workspace workspace = workspaceManager.get(workspaceName);
-        
-        //Check if authorization is necessary
-        if(workspace.getConfig().getMode() == Access.PRIVATE || workspace.getConfig().getMode() == Access.PROTECTED) {
-        	
-            // Get the Authorization header from the request
-            String authorizationHeader =
-                    requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-
-            // Validate the Authorization header
-            if (!isTokenBasedAuthentication(authorizationHeader)) {
-                abortWithUnauthorized(requestContext);
+    public void filter(ContainerRequestContext requestContext) {
+        try {
+            Workspace workspace = workspaceManager.get(AccessFilterHelper.matchPath(requestContext));
+            if (workspace.getConfig().getMode() != Access.PRIVATE && workspace.getConfig().getMode() != Access.PROTECTED) {
                 return;
             }
 
-            // Extract the token from the Authorization header
-            String token = authorizationHeader.substring(AUTHENTICATION_SCHEME.length()).trim();
+            AccessFilterHelper.validateAuthorizationHeader(workspace, requestContext.getHeaderString(HttpHeaders.AUTHORIZATION));
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
 
-            try {
-                // Validate the token
-                validateToken(token, workspace);
-            } catch (Exception e) {
-                abortWithUnauthorized(requestContext);
-            }
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
     }
-
-    private boolean isTokenBasedAuthentication(String authorizationHeader) {
-
-        // Check if the Authorization header is valid
-        // It must not be null and must be prefixed with "Bearer" plus a whitespace
-        // The authentication scheme comparison must be case-insensitive
-        return authorizationHeader != null && authorizationHeader.toLowerCase()
-                    .startsWith(AUTHENTICATION_SCHEME.toLowerCase() + " ");
-    }
-
-    private void abortWithUnauthorized(ContainerRequestContext requestContext) {
-
-        // Abort the filter chain with a 401 status code response
-        // The WWW-Authenticate header is sent along with the response
-        requestContext.abortWith(
-                Response.status(Response.Status.UNAUTHORIZED)
-                        .header(HttpHeaders.WWW_AUTHENTICATE, 
-                                AUTHENTICATION_SCHEME + " realm=\"" + REALM + "\"")
-                        .build());
-    }
-
-    private void validateToken(String token, Workspace workspace) throws Exception {
-    	//TODO OAuth
-    	MessageDigest md = MessageDigest.getInstance("SHA-512");
-    	String tokenHash = ByteHelper.byteArrayToHexString(md.digest(token.getBytes("UTF-8")));
-    	if(!tokenHash.contentEquals(workspace.getConfig().getToken())) {
-    		throw new RuntimeException();
-    	}
-    }
-
 }
---
+++ server/src/main/java/de/materna/dmn/tester/servlets/filters/helpers/AccessFilterHelper.java
@@ -0,0 +1,52 @@
+package de.materna.dmn.tester.servlets.filters.helpers;
+
+import de.materna.dmn.tester.helpers.ByteHelper;
+import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AccessFilterHelper {
+	private static final String AUTHENTICATION_SCHEME = "Bearer";
+
+	private static Pattern pattern = Pattern.compile("/workspaces/(.*?)/");
+
+	public static String matchPath(ContainerRequestContext requestContext) throws MalformedURLException {
+		Matcher matcher = pattern.matcher(requestContext.getUriInfo().getRequestUri().toURL().getPath());
+		if (!matcher.find()) {
+			throw new RuntimeException();
+		}
+
+		return matcher.group(1);
+	}
+
+	/**
+	 * Checks if the Authorization header is valid.
+	 * It must not be null and must be prefixed with "Bearer ".
+	 *
+	 * @param authorizationHeader Authorization Header
+	 */
+	public static void validateAuthorizationHeader(Workspace workspace, String authorizationHeader) throws NoSuchAlgorithmException {
+		if (authorizationHeader == null) {
+			throw new RuntimeException("The authorization header is not set.");
+		}
+
+		// The authentication scheme comparison must be case-insensitive.
+		if (!authorizationHeader.toLowerCase().startsWith(AUTHENTICATION_SCHEME.toLowerCase() + " ")) {
+			throw new RuntimeException("The authorization header doesn't include a bearer token.");
+		}
+
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+
+		String authorizationToken = authorizationHeader.substring(AUTHENTICATION_SCHEME.length() + 1);
+		String tokenHash = ByteHelper.byteArrayToHexString(messageDigest.digest(authorizationToken.getBytes(StandardCharsets.UTF_8)));
+		if (!tokenHash.equals(workspace.getConfig().getToken())) {
+			throw new RuntimeException("The authorization token is not valid.");
+		}
+	}
+}
