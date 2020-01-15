e85269aeab3c4ab9f4719e4e22ddda9d810087f3
Jonas Tamimi <oas@users.noreply.github.com>
2020-01-15 12:19:02 +0100

    Refactored AccessLog.java, removed unused methods

--- server/src/main/java/de/materna/dmn/tester/servlets/workspace/beans/AccessLog.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/workspace/beans/AccessLog.java
@@ -1,76 +1,69 @@
 package de.materna.dmn.tester.servlets.workspace.beans;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import de.materna.dmn.tester.persistence.PersistenceFileManager;
 import de.materna.jdec.serialization.SerializationHelper;
 
-public class AccessLog {
-	private ArrayList<AccessLogEntry> log = new ArrayList<AccessLogEntry>();
-	private final int logLength = 1000;
-	private PersistenceFileManager fileManager;
+import java.io.IOException;
+import java.util.ArrayList;
 
-	//Empty constructor for SerializationHelper
+public class AccessLog {
+	private PersistenceFileManager fileManager;
+	private ArrayList<AccessLogEntry> log = new ArrayList<>();
+	private final int logLength = 1000;
+
 	public AccessLog() {
-		
 	}
-	
-	public AccessLog(String name, PersistenceFileManager fileManager) throws IOException {
+
+	public AccessLog(PersistenceFileManager fileManager) throws IOException {
 		this.fileManager = fileManager;
-		
-		//Import previous access log if it exists
-		if(fileManager.fileExists()) {
-			deserializeFromJson(fileManager.getFile());
+
+		// Import previous access log if it exists.
+		if (fileManager.fileExists()) {
+			deserialize(fileManager.getFile());
 		}
 	}
-	
+
 	public void writeMessage(String message, long timestamp) {
-		if(log.size() > logLength) {
-			trimLog(log);
+		if (log.size() > logLength) {
+			trim(log);
 		}
-		if(log.size() == logLength) {
+		if (log.size() == logLength) {
 			log.remove(0);
 		}
-		
+
 		log.add(new AccessLogEntry(timestamp, message));
-		serializeToJson();
+		serialize();
 	}
-	
-	public String printAsJson() {
-		String json = (String) SerializationHelper.getInstance().toJSON(this);
-		return json;
+
+	public void trim(ArrayList<AccessLogEntry> logTemp) {
+		int diff = logTemp.size() - logLength;
+		if (diff > 0) {
+			logTemp.subList(0, diff).clear();
+		}
 	}
-	
-	public void serializeToJson() {
+
+	public void serialize() {
 		try {
-			fileManager.persistFile(printAsJson());
-		} catch (IOException e) {
+			fileManager.persistFile(SerializationHelper.getInstance().toJSON(this));
+		}
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void deserializeFromJson(String body) {
+
+	public void deserialize(String body) {
 		AccessLog input = (AccessLog) SerializationHelper.getInstance().toClass(body, AccessLog.class);
-		
+
 		ArrayList<AccessLogEntry> logTemp = input.getLog();
-		if(logTemp == null) {
+		if (logTemp == null) {
 			throw new RuntimeException();
-		} else {
+		}
+		else {
 			//If previous log is bigger than currently allowed size, trim it down
-			if(logTemp.size() > logLength) {
-				trimLog(logTemp);
+			if (logTemp.size() > logLength) {
+				trim(logTemp);
 			}
 			this.setLog(logTemp);
-		}
-	}
-	
-	public void trimLog(ArrayList<AccessLogEntry> logTemp) {
-		int diff = logTemp.size() - logLength;
-		for(int i = 0; i < diff; i++) {
-			logTemp.remove(0);
 		}
 	}
 	
@@ -81,27 +74,20 @@ public class AccessLog {
 	public void setLog(ArrayList<AccessLogEntry> log) {
 		this.log = log;
 	}
-	
-	@JsonIgnore
-	public int getLogLength() {
-		return logLength;
-	}
-	
+
 	public static class AccessLogEntry {
 		private long timestamp;
 		private String message;
-		//TODO String data (for metadata or changes)
-		
-		//Empty constructor for SerializationHelper
+		// TODO: String data (for metadata or changes)?
+
 		public AccessLogEntry() {
-			
 		}
-		
+
 		public AccessLogEntry(long timestamp, String message) {
 			this.timestamp = timestamp;
 			this.message = message;
 		}
-		
+
 		public long getTimestamp() {
 			return timestamp;
 		}
@@ -117,15 +103,5 @@ public class AccessLog {
 		public void setMessage(String message) {
 			this.message = message;
 		}
-	}
-
-	@JsonIgnore
-	public PersistenceFileManager getFileManager() {
-		return fileManager;
-	}
-
-	@JsonIgnore
-	public void setFileManager(PersistenceFileManager fileManager) {
-		this.fileManager = fileManager;
 	}
 }
