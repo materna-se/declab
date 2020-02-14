bf55d216e4b18ccebb12798f20a8ab3f5015ecfa
Jonas Tamimi <oas@users.noreply.github.com>
2020-02-14 17:20:59 +0100

    Added DroolsHelper.cleanResult() to remove user-defined functions

--- server/src/main/java/de/materna/dmn/tester/drools/DroolsExecutor.java
+++ server/src/main/java/de/materna/dmn/tester/drools/DroolsExecutor.java
@@ -3,6 +3,7 @@ package de.materna.dmn.tester.drools;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.materna.dmn.tester.servlets.output.beans.Output;
 import de.materna.jdec.DecisionSession;
+import de.materna.jdec.drools.DroolsHelper;
 import de.materna.jdec.serialization.SerializationHelper;
 import org.apache.log4j.Logger;
 import org.kie.dmn.api.core.DMNModel;
@@ -20,12 +21,11 @@ public class DroolsExecutor {
 	 * Uses evaluateInputs to convert all calculated results into our own class hierarchy.
 	 */
 	public static Map<String, Output> getOutputs(DecisionSession decisionSession, DMNModel model, Map<String, ?> inputs) {
-		ObjectMapper objectMapper = SerializationHelper.getInstance().getJSONMapper();
-
 		Map<String, Output> outputs = new LinkedHashMap<>();
 
+		ObjectMapper objectMapper = SerializationHelper.getInstance().getJSONMapper();
 		for (Map.Entry<String, Object> entry : decisionSession.executeModel(model.getNamespace(), model.getName(), inputs).entrySet()) {
-			outputs.put(entry.getKey(), new Output(objectMapper.valueToTree(entry.getValue())));
+			outputs.put(entry.getKey(), new Output(objectMapper.valueToTree(DroolsHelper.cleanResult(entry.getValue()))));
 		}
 
 		return outputs;
