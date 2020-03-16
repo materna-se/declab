49d9ffa573448f67dea0a8fb19896d40c4885b60
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-16 15:45:42 +0100

    Fixed exception that occured because the DMNRuntime was no longer valid

--- server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
+++ server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
@@ -1,6 +1,7 @@
 package de.materna.dmn.tester.drools.helpers;
 
 import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
+import de.materna.jdec.model.ImportResult;
 import de.materna.jdec.model.ModelImportException;
 import de.materna.jdec.model.ModelNotFoundException;
 import org.kie.dmn.api.core.DMNModel;
@@ -15,16 +16,14 @@ public class DroolsHelper extends de.materna.jdec.dmn.DroolsHelper {
 	 */
 	public static DMNModel getModel(Workspace workspace) throws ModelNotFoundException {
 		try {
-			DMNRuntime runtime = workspace.getDecisionSession().getDMNDecisionSession().getRuntime();
-
-			List<DMNModel> dmnModels = runtime.getModels();
+			List<DMNModel> dmnModels = workspace.getDecisionSession().getDMNDecisionSession().getRuntime().getModels();
 			if (dmnModels.size() != 0) {
 				return dmnModels.get(0);
 			}
 
 			if (workspace.getModelManager().fileExists()) {
-				workspace.getDecisionSession().importModel("main", "main", workspace.getModelManager().getFile());
-				return runtime.getModels().get(0);
+				workspace.getDecisionSession().getDMNDecisionSession().importModel("main", "main", workspace.getModelManager().getFile());
+				return workspace.getDecisionSession().getDMNDecisionSession().getRuntime().getModels().get(0);
 			}
 
 			throw new ModelNotFoundException();
