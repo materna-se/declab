d6af2005c0e3fbcf9b9a89161bd99011a6a3edf9
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-26 12:50:42 +0100

    Refactored importModels, started with refactoring getModels

--- server/src/main/java/de/materna/dmn/tester/servlets/model/ModelServlet.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/model/ModelServlet.java
@@ -1,25 +1,20 @@
 package de.materna.dmn.tester.servlets.model;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import de.materna.dmn.tester.drools.DroolsExecutor;
 import de.materna.dmn.tester.drools.helpers.DroolsHelper;
 import de.materna.dmn.tester.persistence.WorkspaceManager;
 import de.materna.dmn.tester.servlets.filters.ReadAccess;
 import de.materna.dmn.tester.servlets.filters.WriteAccess;
 import de.materna.dmn.tester.servlets.input.beans.Decision;
 import de.materna.dmn.tester.servlets.model.beans.Model;
-import de.materna.dmn.tester.servlets.model.beans.ModelResult;
-import de.materna.dmn.tester.servlets.output.beans.Output;
 import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
-import de.materna.jdec.drools.DroolsAnalyzer;
-import de.materna.jdec.drools.DroolsDebugger;
+import de.materna.jdec.dmn.DroolsAnalyzer;
+import de.materna.jdec.model.ExecutionResult;
 import de.materna.jdec.model.ImportResult;
 import de.materna.jdec.model.ModelImportException;
 import de.materna.jdec.serialization.SerializationHelper;
 import org.apache.log4j.Logger;
 import org.kie.dmn.api.core.DMNModel;
-import org.kie.dmn.api.feel.runtime.events.FEELEvent;
-import org.kie.dmn.api.feel.runtime.events.FEELEventListener;
 import org.kie.dmn.feel.FEEL;
 import org.kie.dmn.feel.lang.FEELProfile;
 import org.kie.dmn.feel.parser.feel11.profiles.KieExtendedFEELProfile;
@@ -37,66 +32,54 @@ public class ModelServlet {
 	@ReadAccess
 	@Path("/model")
 	@Produces("application/json")
-	public Response getModels(@PathParam("workspace") String workspaceUUID) {
-		try {
-			Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
+	public Response getModels(@PathParam("workspace") String workspaceUUID) throws IOException {
+		Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
 
-			List<DMNModel> dmnModels = DroolsHelper.getModels(workspace);
-			HashMap<String, Model> modelsMap = new HashMap<String, Model>();
-			LinkedList<HashMap<String, String>> models = workspace.getConfig().getModels();
+		// TODO: Refactor?
+		List<DMNModel> dmnModels = DroolsHelper.getModels(workspace);
+		List<Map<String, String>> models = workspace.getConfig().getModels();
 
-			for(int i = 0; i < dmnModels.size(); i++) {
-				DMNModel dmnModel = dmnModels.get(i);
-				Model model = new Model(dmnModel.getNamespace(), dmnModel.getName(), dmnModel.getDecisions(), dmnModel.getBusinessKnowledgeModels(), dmnModel.getDecisionServices(), workspace.getDecisionSession().getModel(models.get(i).get("namespace"), models.get(i).get("name")));
-				modelsMap.put(models.get(i).get("uuid"), model);
-			}
-
-			return Response.status(Response.Status.OK).entity(SerializationHelper.getInstance().toJSON(modelsMap)).build();
+		HashMap<String, Model> modelsMap = new HashMap<>();
+		for (int i = 0; i < dmnModels.size(); i++) {
+			DMNModel dmnModel = dmnModels.get(i);
+			Model model = new Model(dmnModel.getNamespace(), dmnModel.getName(), dmnModel.getDecisions(), dmnModel.getBusinessKnowledgeModels(), dmnModel.getDecisionServices(), workspace.getDecisionSession().getModel(models.get(i).get("namespace"), models.get(i).get("name")));
+			modelsMap.put(models.get(i).get("uuid"), model);
 		}
-		catch (IOException exception) {
-			return Response.status(Response.Status.OK).entity("{}").build();
-		}
-		catch (IndexOutOfBoundsException exception) {
-			return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Workspace is corrupted.").build();
-		}
+		return Response.status(Response.Status.OK).entity(SerializationHelper.getInstance().toJSON(modelsMap)).build();
 	}
 
 	@PUT
 	@WriteAccess
 	@Path("/model")
 	@Consumes("application/json")
-	public Response importModels(@PathParam("workspace") String workspaceUUID, String body) {
+	public Response importModels(@PathParam("workspace") String workspaceUUID, String body) throws IOException {
 		try {
-			//Initialize
 			Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
 
-			HashMap<String, String>[] params = SerializationHelper.getInstance().toClass(body, new TypeReference<LinkedHashMap<String, String>[]>() {});
-			ImportResult importResult = null;
-			LinkedList<HashMap<String, String>> modelImportOrder = new LinkedList<HashMap<String, String>>();
+			List<Map<String, String>> models = SerializationHelper.getInstance().toClass(body, new TypeReference<List<Map<String, String>>>() {
+			});
 
-			//Clear decision session
+			// Clear decision session and the corresponding directory.
 			workspace.clearDecisionSession();
-
-			//Clear model directory
 			workspace.getModelManager().removeAllFiles();
 
-			//Import models, update configuration
-			for(int i = 0; i < params.length; i++) {
-				HashMap<String, String> modelMap = params[i];
+			ImportResult importResult = new ImportResult();
 
-				importResult = workspace.getDecisionSession().importModel(modelMap.get("namespace"), modelMap.get("name"), modelMap.get("source"));
+			LinkedList<Map<String, String>> importedModels = new LinkedList<>();
+			// Import the provided models, collect all import messages.
+			for (Map<String, String> model : models) {
+				importResult.getMessages().addAll(workspace.getDecisionSession().importModel(model.get("namespace"), model.get("name"), model.get("source")).getMessages());
 
-				String modelUUID = UUID.randomUUID().toString();
+				String uuid = UUID.randomUUID().toString();
+				workspace.getModelManager().persistFile(uuid, model.get("source"));
+				model.put("uuid", uuid);
+				model.remove("source");
 
-				workspace.getModelManager().persistFileRaw(modelUUID, modelMap.get("source"));
-				modelMap.put("uuid", modelUUID);
-				modelMap.remove("source");
-				modelImportOrder.add(modelMap);
+				importedModels.add(model);
 			}
+			workspace.getConfig().setModels(importedModels);
 
-			workspace.getConfig().setModels(modelImportOrder);
-
-			//Update configuration and access log
+			// Update the configuration and add an access log entry.
 			workspace.getConfig().setModifiedDate(System.currentTimeMillis());
 			workspace.getConfig().serialize();
 			workspace.getAccessLog().writeMessage("Imported models", workspace.getConfig().getModifiedDate());
@@ -106,9 +89,6 @@ public class ModelServlet {
 		}
 		catch (ModelImportException exception) {
 			return Response.status(Response.Status.BAD_REQUEST).entity(SerializationHelper.getInstance().toJSON(exception.getResult())).build();
-		}
-		catch (IOException exception) {
-			return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
 		}
 	}
 
@@ -128,21 +108,14 @@ public class ModelServlet {
 	@Path("/model/execute")
 	@Consumes("application/json")
 	@Produces("application/json")
-	public Response calculateModelResult(@PathParam("workspace") String workspaceUUID, String body) {
-		try {
-			Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
+	public Response calculateModelResult(@PathParam("workspace") String workspaceUUID, String body) throws IOException {
+		Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
 
-			Map<String, Object> inputs = SerializationHelper.getInstance().toClass(body, new TypeReference<HashMap<String, Object>>() {
-			});
+		Map<String, Object> inputs = SerializationHelper.getInstance().toClass(body, new TypeReference<HashMap<String, Object>>() {
+		});
 
-			ExecutionResult executionResult = workspace.getDecisionSession().executeModel(DroolsHelper.getModel(workspace), inputs);
-			return Response.status(Response.Status.OK).entity(SerializationHelper.getInstance().toJSON(executionResult)).build();
-		}
-		catch (IOException exception) {
-			log.error(exception);
-
-			return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
-		}
+		ExecutionResult executionResult = workspace.getDecisionSession().executeModel(DroolsHelper.getModel(workspace), inputs);
+		return Response.status(Response.Status.OK).entity(SerializationHelper.getInstance().toJSON(executionResult)).build();
 	}
 
 	@POST
