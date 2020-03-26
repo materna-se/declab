cee6626f72b16b6c07ca410bf01e46e60a7f9e6e
Donaldduck8 <donaldgit@yandex.com>
2020-03-26 11:38:31 +0100

    Update DroolsHelper to support multiple models
    Added getModels() and initModels(), and refactored getModel() to always return the "main" model, i.e. the model that tests will be run against.

--- server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
+++ server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
@@ -6,20 +6,38 @@ import de.materna.jdec.model.ModelNotFoundException;
 import org.kie.dmn.api.core.DMNModel;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class DroolsHelper {
-	public static DMNModel getModel(Workspace workspace) throws IOException {
+	public static List<DMNModel> getModels(Workspace workspace) throws IOException {
 		List<DMNModel> dmnModels = workspace.getDecisionSession().getRuntime().getModels();
 		if (dmnModels.size() != 0) {
-			return dmnModels.get(0);
+			return dmnModels;
+		} else {
+			throw new ModelNotFoundException();
 		}
+	}
 
-		if(workspace.getModelManager().fileExists()) {
-			workspace.getDecisionSession().importModel("main", "main", workspace.getModelManager().getFile());
-			return workspace.getDecisionSession().getRuntime().getModels().get(0);
+	public static DMNModel getModel(Workspace workspace) throws IOException {
+		if(workspace.getConfig().getModels().size() > 0) {
+			LinkedList<HashMap<String, String>> models = workspace.getConfig().getModels();
+			
+			return workspace.getDecisionSession().getRuntime().getModel(models.getLast().get("namespace"), models.getLast().get("name"));
 		}
-
 		throw new ModelNotFoundException();
+	}
+	
+	public static void initModels(Workspace workspace) throws IOException {
+		//Respect model import order if applicable
+		Map<String, String> modelFiles = workspace.getModelManager().getFiles();
+		LinkedList<HashMap<String, String>> models = workspace.getConfig().getModels();
+		if(modelFiles != null && modelFiles.size() > 0 && models != null && models.size() > 0) {
+			for(HashMap<String, String> model : models) {
+				workspace.getDecisionSession().importModel(model.get("namespace"), model.get("name"), modelFiles.get(model.get("uuid")));
+			}
+		}
 	}
 }
