750ef6ee0c29db5ebeef55fc47519ca1cac5caab
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-26 12:49:58 +0100

    Switched to interfaces in Configuration.java

--- server/src/main/java/de/materna/dmn/tester/servlets/workspace/beans/Configuration.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/workspace/beans/Configuration.java
@@ -2,16 +2,13 @@ package de.materna.dmn.tester.servlets.workspace.beans;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import de.materna.dmn.tester.helpers.HashingHelper;
 import de.materna.dmn.tester.persistence.PersistenceFileManager;
 import de.materna.jdec.serialization.SerializationHelper;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Configuration extends PublicConfiguration {
 	private PersistenceFileManager fileManager;
@@ -20,7 +17,7 @@ public class Configuration extends PublicConfiguration {
 	private String salt = null;
 	private long createdDate = Long.MIN_VALUE;
 	private long modifiedDate = Long.MIN_VALUE;
-	private LinkedList<HashMap<String, String>> models = new LinkedList<HashMap<String, String>>();
+	private List<Map<String, String>> models = new LinkedList<>();
 
 	public Configuration() {
 	}
@@ -28,7 +25,7 @@ public class Configuration extends PublicConfiguration {
 	public Configuration(PersistenceFileManager fileManager) throws IOException {
 		this.fileManager = fileManager;
 
-		if(fileManager.fileExists()) {
+		if (fileManager.fileExists()) {
 			fromJson(fileManager.getFile());
 		}
 	}
@@ -42,7 +39,7 @@ public class Configuration extends PublicConfiguration {
 		}
 	}
 
-	
+
 	@JsonIgnore
 	public PublicConfiguration getPublicConfig() {
 		PublicConfiguration pubconfig = new PublicConfiguration();
@@ -60,7 +57,7 @@ public class Configuration extends PublicConfiguration {
 	public void setToken(String token) {
 		this.token = token;
 	}
-	
+
 	@JsonProperty
 	public String getSalt() {
 		return salt;
@@ -90,17 +87,17 @@ public class Configuration extends PublicConfiguration {
 	public void setModifiedDate(long modifiedDate) {
 		this.modifiedDate = modifiedDate;
 	}
-	
+
 	@JsonProperty
-	public LinkedList<HashMap<String, String>> getModels() {
+	public List<Map<String, String>> getModels() {
 		return models;
 	}
 
 	@JsonProperty
-	public void setModels(LinkedList<HashMap<String, String>> modelImportOrder) {
-		this.models = modelImportOrder;
+	public void setModels(List<Map<String, String>> models) {
+		this.models = models;
 	}
-	
+
 	@Override
 	public void fromJson(String body) {
 		Configuration temp = (Configuration) SerializationHelper.getInstance().toClass(body, Configuration.class);
