33118f8706abb678621ee1f421c81d7bae289afe
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-27 15:42:46 +0100

    Refactored DroolsHelper.java, getModels() no preserves the model import order

--- server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
+++ server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
@@ -3,6 +3,7 @@ package de.materna.dmn.tester.drools.helpers;
 import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
 import de.materna.jdec.model.ModelNotFoundException;
 import org.kie.dmn.api.core.DMNModel;
+import org.kie.dmn.api.core.DMNRuntime;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -10,29 +11,35 @@ import java.util.List;
 import java.util.Map;
 
 public class DroolsHelper extends de.materna.jdec.dmn.DroolsHelper {
-	public static List<DMNModel> getModels(Workspace workspace) throws ModelNotFoundException {
-		List<DMNModel> dmnModels = workspace.getDecisionSession().getRuntime().getModels();
-		return dmnModels.size() == 0 ? new LinkedList<>() : dmnModels;
+	public static List<DMNModel> getModels(Workspace workspace) {
+		DMNRuntime runtime = workspace.getDecisionSession().getRuntime();
+
+		List<DMNModel> models = new LinkedList<>();
+
+		List<Map<String, String>> importedModels = workspace.getConfig().getModels();
+		for (Map<String, String> importedModel : importedModels) {
+			models.add(runtime.getModel(importedModel.get("namespace"), importedModel.get("name")));
+		}
+
+		return models;
 	}
 
 	public static DMNModel getModel(Workspace workspace) throws IOException {
-		if (workspace.getConfig().getModels().size() > 0) {
-			List<Map<String, String>> models = workspace.getConfig().getModels();
-
-			Map<String, String> mainModel = models.get(models.size() - 1); // TODO: The main model should be marked explicitly!
-			return workspace.getDecisionSession().getRuntime().getModel(mainModel.get("namespace"), mainModel.get("name"));
+		if (workspace.getConfig().getModels().size() == 0) {
+			throw new ModelNotFoundException();
 		}
-		throw new ModelNotFoundException();
+
+		List<Map<String, String>> importedModels = workspace.getConfig().getModels();
+		Map<String, String> mainImportedModel = importedModels.get(importedModels.size() - 1); // TODO: The main model should be marked explicitly!
+		return workspace.getDecisionSession().getRuntime().getModel(mainImportedModel.get("namespace"), mainImportedModel.get("name"));
 	}
 
 	public static void initModels(Workspace workspace) throws IOException {
-		//Respect model import order if applicable
 		Map<String, String> modelFiles = workspace.getModelManager().getFiles();
+
 		List<Map<String, String>> models = workspace.getConfig().getModels();
-		if (modelFiles != null && modelFiles.size() > 0 && models != null && models.size() > 0) {
-			for (Map<String, String> model : models) {
-				workspace.getDecisionSession().importModel(model.get("namespace"), model.get("name"), modelFiles.get(model.get("uuid")));
-			}
+		for (Map<String, String> model : models) {
+			workspace.getDecisionSession().importModel(model.get("namespace"), model.get("name"), modelFiles.get(model.get("uuid")));
 		}
 	}
 }
