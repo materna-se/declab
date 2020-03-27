bb87a2d42724bdc128c1fe070a7a65f265c0a9e7
Jonas Tamimi <oas@users.noreply.github.com>
2020-03-27 15:50:01 +0100

    Added listener for replacing a model instead of deleting and adding them again

--- client/src/views/model.vue
+++ client/src/views/model.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
+		{{models.map(value => value.name)}}
 		<draggable class="row mb-4" v-model="importedModels" animation="150" draggable=".draggable" v-on:update="orderModels">
-			<div class="col-4 mb-4 draggable" v-for="(importedModel, index) of importedModels">
+			<div class="col-4 mb-4 draggable" v-on:drop="onReplaceModelDrop($event, index)" v-on:dragover="onModelDragOver" v-on:dragenter="onModelDragOver" v-for="(importedModel, index) of importedModels">
 				<div class="card">
 					<div class="card-header d-flex align-items-center">
 						<h4 class="mb-0 mr-auto">{{importedModel.name}}</h4>
@@ -16,19 +17,19 @@
 						</div>
 						<div style="clear: both"></div>
 						<h5 class="mt-2 mb-2" style="clear: both">Inputs</h5>
-						<div class="dmn dmn-input mr-2 mb-2" v-for="(input, key) in importedModel.inputs.value">
-							{{key}}
+						<div class="dmn dmn-input mr-2 mb-2" v-for="input in importedModel.inputs">
+							{{input}}
 						</div>
 						<div style="clear: both"></div>
 						<h5 class="mt-2 mb-2">Business Knowledge Models</h5>
-						<div class="dmn dmn-bkm mr-2 mb-2" v-for="knowledgeModel in importedModel.knowledgeModels">
+						<div class="dmn dmn-bkm mr-2 mb-2" v-for="knowledgeModel in importedModel.knowledgeModels">t
 							{{knowledgeModel}}
 						</div>
 					</div>
 				</div>
 			</div>
 
-			<div class="col-4 mb-4" v-on:drop="onDrop" v-on:dragover="onDragOver" v-on:dragenter="onDragOver">
+			<div class="col-4 mb-4" v-on:drop="onAddModelsDrop" v-on:dragover="onModelDragOver" v-on:dragenter="onModelDragOver">
 				<div class="card" style="height: 100%; min-height: 191px"> <!-- 191 is the height of a model without decisions, inputs and knowledge models -->
 					<div class="card-body text-muted d-flex justify-content-center align-items-center">
 						<div class="d-flex flex-column align-items-center">
@@ -102,44 +103,13 @@
 					});
 					importedModels.push({
 						name: model.name,
-						inputs: await Network.getModelInputs(),
+						inputs: model.inputs,
 						decisions: model.decisions,
 						knowledgeModels: model.knowledgeModels
 					});
 				}
 				this.models = models;
 				this.importedModels = importedModels;
-			},
-			async onDrop(e) {
-				e.preventDefault();
-				if (e.dataTransfer.files.length === 0) {
-					return;
-				}
-
-				this.addModels(e);
-			},
-			async onDragOver(e) {
-				e.preventDefault(); // See https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#droptargets.
-			},
-			async addModels(event) {
-				for (const file of event.dataTransfer.files) {
-					this.models.push(await ((file) => {
-						return new Promise(resolve => {
-							const fileReader = new FileReader();
-							fileReader.addEventListener("load", async function (readerEvent) {
-								const xml = readerEvent.target.result;
-								resolve({
-									name: xml.match(/name="(.+?)"/)[1],
-									namespace: xml.match(/namespace="(.+?)"/)[1],
-									source: xml
-								});
-							});
-							fileReader.readAsText(file, "UTF-8");
-						});
-					})(file));
-				}
-
-				this.importModels();
 			},
 			async importModels() {
 				this.$root.displayAlert(null, null);
@@ -154,12 +124,6 @@
 			deleteModel(index) {
 				this.$delete(this.models, index);
 				this.$delete(this.importedModels, index);
-				this.importModels();
-			},
-			orderModels(e) {
-				const old = this.models[e.oldIndex];
-				this.models[e.oldIndex] = this.models[e.newIndex];
-				this.models[e.newIndex] = old;
 				this.importModels();
 			},
 
@@ -185,7 +149,52 @@
 					message: "The model could not be imported, the following errors have occurred:",
 					state: "danger"
 				};
-			}
+			},
+			loadFile(file) {
+				return new Promise(resolve => {
+					const fileReader = new FileReader();
+					fileReader.addEventListener("load", async function (readerEvent) {
+						const xml = readerEvent.target.result;
+						resolve({
+							name: xml.match(/name="(.+?)"/)[1],
+							namespace: xml.match(/namespace="(.+?)"/)[1],
+							source: xml
+						});
+					});
+					fileReader.readAsText(file, "UTF-8");
+				});
+			},
+
+			// Helpers: Drag and Drop
+			async onModelDragOver(e) {
+				e.preventDefault(); // See https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#droptargets.
+			},
+			async onAddModelsDrop(event) {
+				event.preventDefault();
+				if (event.dataTransfer.files.length === 0) {
+					return;
+				}
+
+				for (const file of event.dataTransfer.files) {
+					this.models.push(await this.loadFile(file));
+				}
+
+				this.importModels();
+			},
+			async onReplaceModelDrop(event, index) {
+				event.preventDefault();
+				if (event.dataTransfer.files.length !== 1) {
+					return;
+				}
+
+				this.models[index] = await this.loadFile(event.dataTransfer.files[0]);
+
+				this.importModels();
+			},
+			orderModels(e) {
+				this.models.splice(e.newIndex, 0, this.models.splice(e.oldIndex, 1)[0]);
+				this.importModels();
+			},
 		}
 	};
 </script>
