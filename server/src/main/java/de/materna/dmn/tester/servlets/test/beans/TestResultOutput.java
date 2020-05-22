39b53efd1fa3a83df603961ff2ba86e2c544534f
Jonas Tamimi <oas@users.noreply.github.com>
2020-05-22 16:22:09 +0200

    Fixed bug that appeared when test result is null

--- server/src/main/java/de/materna/dmn/tester/servlets/test/beans/TestResultOutput.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/test/beans/TestResultOutput.java
@@ -51,6 +51,10 @@ public class TestResultOutput extends Serializable {
 
 	@JsonProperty
 	public boolean isEqual() {
+		if (expected instanceof NullNode || calculated == null) {
+			return true;
+		}
+
 		return expected.equals((expected, calculated) -> {
 			// JSON has only one data type for numbers.
 			// Because of this, we need to make sure that 1 and 1.0 are equal.
