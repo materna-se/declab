720074fadbf107a9df272eaa1580695da35cb110
Jonas Tamimi <oas@users.noreply.github.com>
2020-06-05 15:48:11 +0200

    Added check if context for decision exists

--- client/src/views/builder.vue
+++ client/src/views/builder.vue
@@ -73,7 +73,7 @@
 							<h5 class="mb-2">Output</h5>
 							<json-builder class="mb-0" v-bind:template="output" v-bind:convert="true" v-bind:fixed="true" v-bind:fixed-values="true"/>
 
-							<div class="mt-4" v-if="Object.keys(model.result.context[key]).length !== 0">
+							<div class="mt-4" v-if="model.result.context[key] !== undefined && Object.keys(model.result.context[key]).length !== 0">
 								<h5 class="mb-2">Context</h5>
 								<json-builder class="mb-0" v-bind:template="model.result.context[key]" v-bind:convert="true" v-bind:fixed="true" v-bind:fixed-values="true"/>
 							</div>
