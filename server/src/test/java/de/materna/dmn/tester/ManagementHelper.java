dd4cda5ae19439b775ff220088abd0ed76616c35
Jonas Tamimi <oas@users.noreply.github.com>
2020-07-16 18:00:37 +0200

    Bumped version to 2.3.0

--- server/src/test/java/de/materna/dmn/tester/ManagementHelper.java
+++ server/src/test/java/de/materna/dmn/tester/ManagementHelper.java
@@ -1,7 +1,7 @@
 package de.materna.dmn.tester;
 
 public class ManagementHelper {
-	public static String declabVersion = "2.0.2";
+	public static String declabVersion = "2.3.0";
 	public static String declabHost = "http://localhost:8080/declab-" + declabVersion;
 	public static int configurationVersion = 2;
 }
