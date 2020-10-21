5e9e779b9d496ba374eae69c59a79ffa25f682ae
Jonas Tamimi <oas@users.noreply.github.com>
2020-10-21 14:02:48 +0200

    Changed footer margin

--- client/src/components/footer.vue
+++ client/src/components/footer.vue
@@ -10,7 +10,7 @@
 				<div class="col-4">
 					<small class="d-block text-right text-muted">
 						Version {{version}}
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" v-tooltip="{content: developerInformation, classes: 'developer'}" style="margin-bottom: 2px">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" v-tooltip="{content: developerInformation, classes: 'developer'}" style="margin-bottom: 3px">
 							<path d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 002 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2m-1 15h2v-6h-2v6z" fill="currentColor"/>
 						</svg>
 					</small>
