af240aaf9565f40b6c5186f429eade1aba21f031
Donaldduck8 <donaldgit@yandex.com>
2021-06-28 11:57:39 +0200

    Add ModelMap bean

---
+++ server/src/main/java/de/materna/dmn/tester/servlets/challenges/beans/ModelMap.java
@@ -0,0 +1,25 @@
+package de.materna.dmn.tester.servlets.challenges.beans;
+
+public class ModelMap {
+	public String namespace;
+	public String name;
+	public String source;
+	public String getNamespace() {
+		return namespace;
+	}
+	public void setNamespace(String namespace) {
+		this.namespace = namespace;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+}
