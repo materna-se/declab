a031d159ce4bcedf23f72b2d8f127e834bb153cd
Jonas Tamimi <oas@users.noreply.github.com>
2021-09-30 13:34:05 +0200

    Updated kie-editors-standalone dependency, bumped version to 2.6.7

--- server/src/test/java/de/materna/dmn/tester/ManagementHelper.java
+++ server/src/test/java/de/materna/dmn/tester/ManagementHelper.java
@@ -1,7 +1,7 @@
 package de.materna.dmn.tester;
 
 public class ManagementHelper {
-	public static String declabVersion = "2.6.0";
+	public static String declabVersion = "2.6.7";
 	public static String declabHost = "http://localhost:8080/declab-" + declabVersion;
 	public static int configurationVersion = 2;
 }
