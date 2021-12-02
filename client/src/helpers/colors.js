dbad9eb7eabbd98cc744b52c1c1653ac2795ce99
Schlömer <donaldgit@yandex.com>
2021-12-02 11:55:21 +0100

    Refactor discovery table colors to reflect the Colour Alphabet Project

--- client/src/helpers/colors.js
+++ client/src/helpers/colors.js
@@ -1,49 +1,30 @@
 export default {
-    pleasingRandomColor() {
-        const golden_ratio_conjugate = 0.618033988749895;
-        let r = Math.random();
+    colorIndex: 0,
 
-        r += golden_ratio_conjugate;
-        r %= 1;
-        return this.HSVtoRGBtoHEX(r, 0.5, 0.95);
+    colors: [
+        "#F0A3FF",
+        "#2BCE48",
+        "#FFCC99",
+        "#808080",
+        "#94FFB5",
+        "#8F7C00",
+        "#9DCC00",
+        "#C20088",
+        "#FFA405",
+        "#FFA8BB",
+        "#426600",
+        "#FF0010",
+        "#5EF1F2",
+        "#00998F",
+        "#E0FF66",
+        "#740AFF",
+        "#FFFF00",
+        "#FF5005"
+    ],
+
+    distinguishableColor() {
+        this.colorIndex = (this.colorIndex + 1) % this.colors.length;
+
+        return this.colors[this.colorIndex];
     },
-
-    HSVtoRGBtoHEX(h, s, v) {
-        var r, g, b, i, f, p, q, t;
-        if (arguments.length === 1) {
-            s = h.s, v = h.v, h = h.h;
-        }
-        i = Math.floor(h * 6);
-        f = h * 6 - i;
-        p = v * (1 - s);
-        q = v * (1 - f * s);
-        t = v * (1 - (1 - f) * s);
-        switch (i % 6) {
-            case 0: r = v, g = t, b = p; break;
-            case 1: r = q, g = v, b = p; break;
-            case 2: r = p, g = v, b = t; break;
-            case 3: r = p, g = q, b = v; break;
-            case 4: r = t, g = p, b = v; break;
-            case 5: r = v, g = p, b = q; break;
-        }
-
-        r = Math.round(r * 255);
-        g = Math.round(g * 255);
-        b = Math.round(b * 255);
-
-        return this.RGBtoHEX(r, g, b);
-    },
-
-    RGBtoHEX(r, g, b) {
-        var rH = r.toString(16);
-        rH = (rH.length == 1 ? "0" + rH : rH);
-
-        var gH = g.toString(16);
-        gH = (gH.length == 1 ? "0" + gH : gH);
-
-        var bH = b.toString(16);
-        bH = (bH.length == 1 ? "0" + bH : bH);
-
-        return "#" + rH + gH + bH;
-    }
 }
