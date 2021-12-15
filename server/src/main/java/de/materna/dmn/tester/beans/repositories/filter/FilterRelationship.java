958f86c065fc151212c9392ed7c50c139efc5729
Wolffgang <georg@wolffgang.de>
2021-12-15 11:27:15 +0100

    Create FilterRelationship.java

---
+++ server/src/main/java/de/materna/dmn/tester/beans/repositories/filter/FilterRelationship.java
@@ -0,0 +1,12 @@
+package de.materna.dmn.tester.beans.repositories.filter;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import de.materna.dmn.tester.beans.Relationship;
+
+public interface FilterRelationship {
+	public Predicate toPredicate(Root<Relationship> root, CriteriaQuery<Relationship> cq, CriteriaBuilder cb);
+}
