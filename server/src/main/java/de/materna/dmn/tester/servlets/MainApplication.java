8d6221675f2dfc23bdb19c60ac781197e2cb4dce
Wolffgang <georg@wolffgang.de>
2021-12-15 11:29:14 +0100

    Update MainApplication.java

--- server/src/main/java/de/materna/dmn/tester/servlets/MainApplication.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/MainApplication.java
@@ -1,12 +1,25 @@
 package de.materna.dmn.tester.servlets;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import org.apache.log4j.Logger;
+
+import de.materna.dmn.tester.beans.Database;
+import de.materna.dmn.tester.beans.User;
+import de.materna.dmn.tester.beans.repositories.UserRepository;
 import de.materna.dmn.tester.persistence.WorkspaceManager;
+import de.materna.dmn.tester.servlets.challenges.ChallengeServlet;
 import de.materna.dmn.tester.servlets.exceptions.DefaultOptionsMethodExceptionMapper;
 import de.materna.dmn.tester.servlets.exceptions.GeneralExceptionMapper;
 import de.materna.dmn.tester.servlets.filters.CSRFFilter;
 import de.materna.dmn.tester.servlets.filters.ReadAccessFilter;
 import de.materna.dmn.tester.servlets.filters.WriteAccessFilter;
-import de.materna.dmn.tester.servlets.challenges.ChallengeServlet;
 import de.materna.dmn.tester.servlets.input.InputServlet;
 import de.materna.dmn.tester.servlets.model.ModelServlet;
 import de.materna.dmn.tester.servlets.output.OutputServlet;
@@ -15,16 +28,11 @@ import de.materna.dmn.tester.servlets.test.TestServlet;
 import de.materna.dmn.tester.servlets.workspace.MetaWorkspaceServlet;
 import de.materna.dmn.tester.servlets.workspace.WorkspaceServlet;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
 @ApplicationPath("/api")
 public class MainApplication extends Application {
 	private Set<Object> singletons = new HashSet<>();
 	private Set<Class<?>> classes = new HashSet<>();
+	private static final Logger log = Logger.getLogger(MainApplication.class);
 
 	public MainApplication() throws IOException {
 		// Before we initialize the endpoints, we'll initialize all workspaces.
@@ -42,9 +50,17 @@ public class MainApplication extends Application {
 		classes.add(CSRFFilter.class);
 		classes.add(ReadAccessFilter.class);
 		classes.add(WriteAccessFilter.class);
-		
+
 		classes.add(DefaultOptionsMethodExceptionMapper.class);
 		classes.add(GeneralExceptionMapper.class);
+		
+//		Database db = new Database();
+//		db.connect();
+/*		User newUser = new User();
+		UserRepository ur = new UserRepository();
+		ur.save(newUser);
+		Optional<User> loaded = ur.findByUuid(0);
+		boolean stop = true;*/
 	}
 
 	@Override
