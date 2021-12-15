7d9e47b8f9c9a911636bc45e3d1fb3073e686f13
Jonas Tamimi <oas@users.noreply.github.com>
2021-12-15 13:54:09 +0100

    Removed unnecessary imports from servlets

--- server/src/main/java/de/materna/dmn/tester/servlets/challenges/ChallengeServlet.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/challenges/ChallengeServlet.java
@@ -1,26 +1,5 @@
 package de.materna.dmn.tester.servlets.challenges;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Response;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-
 import de.materna.dmn.tester.persistence.PersistenceDirectoryManager;
 import de.materna.dmn.tester.persistence.WorkspaceManager;
 import de.materna.dmn.tester.servlets.challenges.beans.Challenge;
@@ -28,8 +7,6 @@ import de.materna.dmn.tester.servlets.challenges.beans.Challenge.ChallengeType;
 import de.materna.dmn.tester.servlets.challenges.beans.DMNSolution;
 import de.materna.dmn.tester.servlets.challenges.beans.ModelMap;
 import de.materna.dmn.tester.servlets.challenges.beans.Scenario;
-import de.materna.dmn.tester.servlets.challenges.beans.FEELSolution;
-import de.materna.dmn.tester.servlets.challenges.beans.DMNSolution;
 import de.materna.dmn.tester.servlets.challenges.helpers.ChallengeExecutionHelper;
 import de.materna.dmn.tester.servlets.filters.ReadAccess;
 import de.materna.dmn.tester.servlets.filters.WriteAccess;
@@ -38,9 +15,11 @@ import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
 import de.materna.jdec.model.ModelImportException;
 import de.materna.jdec.model.ModelNotFoundException;
 import de.materna.jdec.serialization.SerializationHelper;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -55,10 +34,10 @@ public class ChallengeServlet {
 		PersistenceDirectoryManager<Challenge> challengeManager = workspace.getChallengeManager();
 
 		Map<String, Challenge> unsortedChallenges = challengeManager.getFiles();
-		
+
 		Map<String, Challenge> sortedChallenges = new LinkedHashMap<>();
 		unsortedChallenges.entrySet().stream().sorted(Map.Entry.comparingByValue((o1, o2) -> (order ? -1 : 1) * o1.getName().compareTo(o2.getName()))).forEach(entry -> sortedChallenges.put(entry.getKey(), entry.getValue()));
-			
+
 		return Response.status(Response.Status.OK).entity(sortedChallenges).build();
 	}
 
@@ -74,7 +53,7 @@ public class ChallengeServlet {
 		if (challenge == null) {
 			throw new NotFoundException();
 		}
-		
+
 		workspace.getAccessLog().writeMessage("Accessed challenge " + challengeUUID, System.currentTimeMillis());
 
 		return Response.status(Response.Status.OK).entity(SerializationHelper.getInstance().toJSON(challenge)).build();
@@ -92,7 +71,8 @@ public class ChallengeServlet {
 
 			scenarios = ChallengeExecutionHelper.calculateFEELExpression(feelExpression, scenarios);
 			challenge.setScenarios(scenarios);
-		} else if (challengeType.equals(ChallengeType.DMN_MODEL)) {
+		}
+		else if (challengeType.equals(ChallengeType.DMN_MODEL)) {
 			// Need to re-parse to avoid ClassCastException
 			DMNSolution solution = (DMNSolution) SerializationHelper.getInstance().toClass(SerializationHelper.getInstance().toJSON(challenge.getSolution()), DMNSolution.class);
 
@@ -103,7 +83,8 @@ public class ChallengeServlet {
 			// Use decision service if available
 			if (decisionService != null) {
 				scenarios = ChallengeExecutionHelper.calculateModels(modelMaps, scenarios, decisionService);
-			} else {
+			}
+			else {
 				scenarios = ChallengeExecutionHelper.calculateModels(modelMaps, scenarios);
 			}
 
@@ -119,7 +100,7 @@ public class ChallengeServlet {
 	public Response createChallenge(@PathParam("workspace") String workspaceUUID, String body) throws IOException, RuntimeException {
 		Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
 		String uuid = UUID.randomUUID().toString();
-		
+
 		try {
 			Challenge challenge = (Challenge) SerializationHelper.getInstance().toClass(body, Challenge.class);
 
@@ -130,7 +111,8 @@ public class ChallengeServlet {
 			workspace.getAccessLog().writeMessage("Created challenge" + uuid, System.currentTimeMillis());
 
 			return Response.status(Response.Status.CREATED).entity(uuid).build();
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(Response.Status.BAD_REQUEST).build();
 		}
@@ -144,7 +126,7 @@ public class ChallengeServlet {
 	public Response editChallenge(@PathParam("workspace") String workspaceUUID, @PathParam("uuid") String challengeUUID, String body) throws IOException {
 		Workspace workspace = WorkspaceManager.getInstance().get(workspaceUUID);
 		PersistenceDirectoryManager<Challenge> challengeManager = workspace.getChallengeManager();
-		
+
 		if (!challengeManager.getFiles().containsKey(challengeUUID)) {
 			throw new NotFoundException();
 		}
