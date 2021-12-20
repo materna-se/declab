ed2d701b65bd534d21c1ef8b84f74d79a8768128
Wolffgang <georg@wolffgang.de>
2021-12-20 19:12:19 +0100

    update package.json

--- server/src/test/java/de/materna/dmn/tester/helpers/MergingHelperTest.java
+++ server/src/test/java/de/materna/dmn/tester/helpers/MergingHelperTest.java
@@ -1,14 +1,12 @@
 package de.materna.dmn.tester.helpers;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class MergingHelperTest {
 	@Test
