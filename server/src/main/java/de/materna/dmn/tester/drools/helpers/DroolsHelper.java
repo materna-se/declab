107b68a424e80b028e6658dc93fda45631537668
Jonas Tamimi <oas@users.noreply.github.com>
2022-01-28 16:46:39 +0100

    Added exception handling to DroolsHelper

--- server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
+++ server/src/main/java/de/materna/dmn/tester/drools/helpers/DroolsHelper.java
@@ -1,26 +1,38 @@
 package de.materna.dmn.tester.drools.helpers;
 
-import de.materna.dmn.tester.persistence.WorkspaceManager;
 import de.materna.dmn.tester.servlets.workspace.beans.Workspace;
 import de.materna.jdec.model.ModelImportException;
+import de.materna.jdec.model.ModelNotFoundException;
+import org.kie.dmn.api.core.DMNModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.kie.dmn.api.core.DMNModel;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class DroolsHelper extends de.materna.jdec.dmn.DroolsHelper {
 	private static final Logger log = LoggerFactory.getLogger(DroolsHelper.class);
 
-	public static String getMainModelNamespace(Workspace workspace) {
-		return workspace.getConfig().getModels().get(workspace.getConfig().getModels().size() - 1).get("namespace");
+	public static String getMainModelNamespace(Workspace workspace) throws ModelNotFoundException {
+		List<Map<String, String>> models = workspace.getConfig().getModels();
+		if (models.size() == 0) {
+			throw new ModelNotFoundException();
+		}
+
+		return models.get(models.size() - 1).get("namespace");
 	}
 
-	public static DMNModel getMainModel(Workspace workspace) {
+	public static DMNModel getMainModel(Workspace workspace) throws ModelNotFoundException {
 		String mainModelNamespace = getMainModelNamespace(workspace);
-		return workspace.getDecisionSession().getDMNDecisionSession().getRuntime().getModels().stream().filter(dmnModel -> dmnModel.getNamespace().equals(mainModelNamespace)).findAny().get();
+
+		Optional<DMNModel> optionalModel = workspace.getDecisionSession().getDMNDecisionSession().getRuntime().getModels().stream().filter(dmnModel -> dmnModel.getNamespace().equals(mainModelNamespace)).findAny();
+		if (!optionalModel.isPresent()) {
+			throw new ModelNotFoundException();
+		}
+
+		return optionalModel.get();
 	}
 
 	public static void importModels(Workspace workspace) throws IOException {
@@ -32,7 +44,7 @@ public class DroolsHelper extends de.materna.jdec.dmn.DroolsHelper {
 				workspace.getDecisionSession().importModel(model.get("namespace"), modelFiles.get(model.get("uuid")));
 			}
 			catch (Exception e) {
-				if(e instanceof ModelImportException) {
+				if (e instanceof ModelImportException) {
 					log.warn("Import of model " + model.get("namespace") + " failed!" + ((ModelImportException) e).getResult().getMessages());
 				}
 				else {
