7e21f874c8cf301e93cddf28b6e09cd70ceebb0a
Wolffgang <georg@wolffgang.de>
2022-05-06 14:59:31 +0200

    Update LaboratoryRepository.java
    Added update-method

--- server/src/main/java/de/materna/dmn/tester/interfaces/repositories/LaboratoryRepository.java
+++ server/src/main/java/de/materna/dmn/tester/interfaces/repositories/LaboratoryRepository.java
@@ -3,6 +3,8 @@ package de.materna.dmn.tester.interfaces.repositories;
 import java.util.List;
 import java.util.UUID;
 
+import javax.xml.registry.JAXRException;
+
 import de.materna.dmn.tester.beans.laboratory.Laboratory;
 import de.materna.dmn.tester.enums.VisabilityType;
 
@@ -21,6 +23,9 @@ public interface LaboratoryRepository {
 	Laboratory put(Laboratory laboratory);
 
 	Laboratory create(String name, String description, VisabilityType visability);
+
+	Laboratory update(UUID laboratoryUuid, String name, String description, VisabilityType visability)
+			throws JAXRException;
 
 	boolean delete(Laboratory laboratory);
 }
