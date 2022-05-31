b720982f6f625c5ba9add662f237f86684bef0fb
Jonas Tamimi <oas@users.noreply.github.com>
2022-05-31 11:27:49 +0200

    Updated header design

--- client/src/components/header.vue
+++ client/src/components/header.vue
@@ -16,16 +16,16 @@
 			</li>
 		</ul>
 
-		<div class="nav-link" style="display: flex; flex-direction: column">
+		<div class="nav-link" style="display: flex; flex-direction: column; padding: 0.2rem 1rem">
 			<small v-if="$store.state.name !== null">
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-					<path d="M20 18H4V8h16m0-2h-8l-2-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2Z" fill="#f6f6f8"/>
+					<path d="M20 18H4V8h16m0-2h-8l-2-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2Z" fill="currentColor"/>
 				</svg>
 				<span style="line-height: 1">{{$store.state.name}}</span>
 			</small>
 			<small v-if="$route.params.workspace !== undefined && $store.state.listeners !== 0">
 				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
-					<path d="M4.93 4.93A9.969 9.969 0 002 12c0 2.76 1.12 5.26 2.93 7.07l1.41-1.41A7.938 7.938 0 014 12c0-2.21.89-4.22 2.34-5.66L4.93 4.93m14.14 0l-1.41 1.41A7.955 7.955 0 0120 12c0 2.22-.89 4.22-2.34 5.66l1.41 1.41A9.969 9.969 0 0022 12c0-2.76-1.12-5.26-2.93-7.07M7.76 7.76A5.98 5.98 0 006 12c0 1.65.67 3.15 1.76 4.24l1.41-1.41A3.99 3.99 0 018 12c0-1.11.45-2.11 1.17-2.83L7.76 7.76m8.48 0l-1.41 1.41A3.99 3.99 0 0116 12c0 1.11-.45 2.11-1.17 2.83l1.41 1.41A5.98 5.98 0 0018 12c0-1.65-.67-3.15-1.76-4.24M12 10a2 2 0 00-2 2 2 2 0 002 2 2 2 0 002-2 2 2 0 00-2-2z" fill="#f6f6f8"/>
+					<path d="M4.93 4.93A9.969 9.969 0 002 12c0 2.76 1.12 5.26 2.93 7.07l1.41-1.41A7.938 7.938 0 014 12c0-2.21.89-4.22 2.34-5.66L4.93 4.93m14.14 0l-1.41 1.41A7.955 7.955 0 0120 12c0 2.22-.89 4.22-2.34 5.66l1.41 1.41A9.969 9.969 0 0022 12c0-2.76-1.12-5.26-2.93-7.07M7.76 7.76A5.98 5.98 0 006 12c0 1.65.67 3.15 1.76 4.24l1.41-1.41A3.99 3.99 0 018 12c0-1.11.45-2.11 1.17-2.83L7.76 7.76m8.48 0l-1.41 1.41A3.99 3.99 0 0116 12c0 1.11-.45 2.11-1.17 2.83l1.41 1.41A5.98 5.98 0 0018 12c0-1.65-.67-3.15-1.76-4.24M12 10a2 2 0 00-2 2 2 2 0 002 2 2 2 0 002-2 2 2 0 00-2-2z" fill="currentColor"/>
 				</svg>
 				<span style="line-height: 1">{{$store.state.listeners}}</span>
 			</small>
@@ -34,8 +34,7 @@
 		<!-- Manual /settings route -->
 		<router-link class="nav-link" v-if="$route.params.workspace !== undefined" v-bind:to="'/' + $route.params.workspace + '/settings'" aria-label="Settings">
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-				<path d="M12 8a4 4 0 0 1 4 4 4 4 0 0 1-4 4 4 4 0 0 1-4-4 4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 2 2 2 0 0 0 2 2 2 2 0 0 0 2-2 2 2 0 0 0-2-2m-2 12c-.25 0-.46-.18-.5-.42l-.37-2.65c-.63-.25-1.17-.59-1.69-.99l-2.49 1.01c-.22.08-.49 0-.61-.22l-2-3.46a.493.493 0 0 1 .12-.64l2.11-1.66L4.5 12l.07-1-2.11-1.63a.493.493 0 0 1-.12-.64l2-3.46c.12-.22.39-.31.61-.22l2.49 1c.52-.39 1.06-.73 1.69-.98l.37-2.65c.04-.24.25-.42.5-.42h4c.25 0 .46.18.5.42l.37 2.65c.63.25 1.17.59 1.69.98l2.49-1c.22-.09.49 0 .61.22l2 3.46c.13.22.07.49-.12.64L19.43 11l.07 1-.07 1 2.11 1.63c.19.15.25.42.12.64l-2 3.46c-.12.22-.39.31-.61.22l-2.49-1c-.52.39-1.06.73-1.69.98l-.37 2.65c-.04.24-.25.42-.5.42h-4m1.25-18l-.37 2.61c-1.2.25-2.26.89-3.03 1.78L5.44 7.35l-.75 1.3L6.8 10.2a5.55 5.55 0 0 0 0 3.6l-2.12 1.56.75 1.3 2.43-1.04c.77.88 1.82 1.52 3.01 1.76l.37 2.62h1.52l.37-2.61c1.19-.25 2.24-.89 3.01-1.77l2.43 1.04.75-1.3-2.12-1.55c.4-1.17.4-2.44 0-3.61l2.11-1.55-.75-1.3-2.41 1.04a5.42 5.42 0 0 0-3.03-1.77L12.75 4h-1.5z"
-							fill="#f6f6f8"/>
+				<path d="M12 8a4 4 0 0 1 4 4 4 4 0 0 1-4 4 4 4 0 0 1-4-4 4 4 0 0 1 4-4m0 2a2 2 0 0 0-2 2 2 2 0 0 0 2 2 2 2 0 0 0 2-2 2 2 0 0 0-2-2m-2 12c-.25 0-.46-.18-.5-.42l-.37-2.65c-.63-.25-1.17-.59-1.69-.99l-2.49 1.01c-.22.08-.49 0-.61-.22l-2-3.46a.493.493 0 0 1 .12-.64l2.11-1.66L4.5 12l.07-1-2.11-1.63a.493.493 0 0 1-.12-.64l2-3.46c.12-.22.39-.31.61-.22l2.49 1c.52-.39 1.06-.73 1.69-.98l.37-2.65c.04-.24.25-.42.5-.42h4c.25 0 .46.18.5.42l.37 2.65c.63.25 1.17.59 1.69.98l2.49-1c.22-.09.49 0 .61.22l2 3.46c.13.22.07.49-.12.64L19.43 11l.07 1-.07 1 2.11 1.63c.19.15.25.42.12.64l-2 3.46c-.12.22-.39.31-.61.22l-2.49-1c-.52.39-1.06.73-1.69.98l-.37 2.65c-.04.24-.25.42-.5.42h-4m1.25-18l-.37 2.61c-1.2.25-2.26.89-3.03 1.78L5.44 7.35l-.75 1.3L6.8 10.2a5.55 5.55 0 0 0 0 3.6l-2.12 1.56.75 1.3 2.43-1.04c.77.88 1.82 1.52 3.01 1.76l.37 2.62h1.52l.37-2.61c1.19-.25 2.24-.89 3.01-1.77l2.43 1.04.75-1.3-2.12-1.55c.4-1.17.4-2.44 0-3.61l2.11-1.55-.75-1.3-2.41 1.04a5.42 5.42 0 0 0-3.03-1.77L12.75 4h-1.5z" fill="currentColor"/>
 			</svg>
 		</router-link>
 	</nav>
@@ -43,14 +42,23 @@
 
 <style scoped>
 	.navbar,
-	.navbar .dropdown-item:active {
-		background: rgb(57, 62, 70);
-		box-shadow: 0 0 20px 0 rgba(57, 62, 70, 0.5);
+	.dropdown-item.active, .dropdown-item:active {
+		background-color: rgb(255, 255, 255);
+		box-shadow: 0px 2px 8px 0px rgb(133 133 133 / 16%);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.08);
 	}
 
-	.navbar .navbar-brand,
-	.navbar .nav-link {
+	.dropdown-item.active, .dropdown-item:active {
+		background-color: rgb(70, 75, 83);
+		box-shadow: 0px 2px 8px 0px rgb(133 133 133 / 16%);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.08);
 		color: #ffffff;
+	}
+
+	.navbar .nav-link {
+		color: rgb(70, 75, 83);
+		font-weight: 600;
+		font-size: 1.1rem;
 	}
 
 	.navbar .navbar-brand {
@@ -60,19 +68,15 @@
 	}
 
 	.navbar .navbar-brand .navbar-brand-primary {
-		background: rgb(198, 22, 35);
-		color: #ffffff;
+		font-weight: 600;
+		color: rgb(198, 22, 35);
 		line-height: 50px;
-		padding-right: 3px;
-		padding-left: 3px;
 	}
 
 	.navbar .navbar-brand .navbar-brand-secondary {
-		background: rgb(70, 75, 83);
-		color: #ffffff;
+		font-weight: 600;
+		color: rgb(70, 75, 83);
 		line-height: 50px;
-		padding-left: 3px;
-		padding-right: 3px;
 	}
 
 	.dropdown-menu {
@@ -119,6 +123,10 @@
 							{
 								name: "Publisher",
 								path: "publisher"
+							},
+							{
+								name: "Documentation",
+								path: "documentation"
 							},
 							{
 								name: "Discoverer",
