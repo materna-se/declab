1b72b3d81149ba1ac91fbc9134b53a9a669f229b
Jonas Tamimi <oas@users.noreply.github.com>
2022-05-31 11:41:38 +0200

    Added more documentation components

---
+++ client/src/components/documentation/parameter-documentation.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div v-for="(parameter, index) of parameters" class="d-flex align-items-center justify-content-center" v-bind:class="[index === parameters.length - 1 ? null : 'mb-1']">
+			<b class="me-2">{{parameter.name}}</b>
+			<type-badge v-bind:type="parameter.type"/>
+		</div>
+	</div>
+</template>
+
+<script>
+	import TypeBadge from "./type-badge";
+
+	export default {
+		name: "parameter-documentation",
+		props: {
+			parameters: {
+				type: Array,
+				required: true
+			},
+		},
+		components: {
+			"type-badge": TypeBadge
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
