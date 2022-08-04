1e6cee5d05655af42abf709d7cbdd0ecf14bac2d
Wolffgang <georg@wolffgang.de>
2022-08-04 15:52:12 +0200

    Update MainApplication.java
    added singletons.add(new PortalServlet());

--- server/src/main/java/de/materna/dmn/tester/servlets/MainApplication.java
+++ server/src/main/java/de/materna/dmn/tester/servlets/MainApplication.java
@@ -1,26 +1,30 @@
 package de.materna.dmn.tester.servlets;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 import de.materna.dmn.tester.persistence.WorkspaceManager;
+import de.materna.dmn.tester.servlets.challenges.ChallengeServlet;
 import de.materna.dmn.tester.servlets.exceptions.mapper.GeneralExceptionMapper;
 import de.materna.dmn.tester.servlets.filters.CSRFFilter;
 import de.materna.dmn.tester.servlets.filters.ReadAccessFilter;
 import de.materna.dmn.tester.servlets.filters.WriteAccessFilter;
-import de.materna.dmn.tester.servlets.challenges.ChallengeServlet;
 import de.materna.dmn.tester.servlets.input.InputServlet;
 import de.materna.dmn.tester.servlets.model.ModelServlet;
 import de.materna.dmn.tester.servlets.output.OutputServlet;
 import de.materna.dmn.tester.servlets.playground.PlaygroundServlet;
+import de.materna.dmn.tester.servlets.portal.PortalServlet;
 import de.materna.dmn.tester.servlets.test.TestServlet;
 import de.materna.dmn.tester.servlets.workspace.MetaWorkspaceServlet;
 import de.materna.dmn.tester.servlets.workspace.WorkspaceServlet;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("/api")
 public class MainApplication extends Application {
@@ -39,6 +43,7 @@ public class MainApplication extends Application {
 		singletons.add(new TestServlet());
 		singletons.add(new MetaWorkspaceServlet());
 		singletons.add(new WorkspaceServlet());
+		singletons.add(new PortalServlet());
 
 		classes.add(CSRFFilter.class);
 		classes.add(ReadAccessFilter.class);
