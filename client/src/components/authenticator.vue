a7e458915a806d3336e930c7c7c7d9db6de060db
Jonas Tamimi <oas@users.noreply.github.com>
2023-11-16 15:37:46 +0100

    Updated dependencies, fixed several small issues

--- client/src/components/authenticator.vue
+++ client/src/components/authenticator.vue
@@ -6,7 +6,7 @@
 				<div class="modal-content p-4">
 					<div class="modal-body">
 						<p class="mb-2">To perform this action, please enter the workspace password.</p>
-						<input placeholder="Enter Password..." class="form-control mb-2" v-on:keyup="updateToken($event.target.value)" v-on:keyup.enter="reload">
+						<input placeholder="Enter Password..." type="password" class="form-control mb-2" v-on:keyup="updateToken($event.target.value)" v-on:keyup.enter="reload">
 						<button type="button" class="btn btn-primary w-100" v-on:click="reload">Save Password</button>
 					</div>
 				</div>
